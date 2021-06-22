cfad6f28b923fdb160e7f5a2c18d5a710e47efdf
james.howes <james.howes@digital.cabinet-office.gov.uk>
2021-06-22 16:36:37 +0100

    LTG-35 - Write unit test for when a user creates password
    Co-authored-by: Daniel Besbrode <daniel.besbrode@digital.cabinet-office.gov.uk>

---
+++ src/components/register-create-password/tests/register-create-password-controller.test.ts
@@ -0,0 +1,105 @@
+import { expect } from "chai";
+import { describe } from "mocha";
+
+import { sinon } from "../../../../tests/utils/test-utils";
+import { NextFunction, Request, Response } from "express";
+import { AuthenticationServiceInterface } from "../../../services/authentication-service.interface";
+import { createPasswordPost, createPasswordGet } from "../register-create-password-controller";
+import { UserSession } from "../../../types";
+import { USER_STATE } from "../../../app.constants";
+
+describe("register-create-password controller", () => {
+  let sandbox: sinon.SinonSandbox;
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: NextFunction;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+
+    req = { body: {}, session: { user: {} as UserSession } };
+    res = { render: sandbox.fake(), redirect: sandbox.fake() };
+    next = sinon.spy();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe("createPasswordGet", () => {
+    it("should render create password view", () => {
+      createPasswordGet(req as Request, res as Response);
+
+      expect(res.render).to.have.calledWith("register-create-password/index.njk");
+    });
+  });
+
+  describe("createPasswordPost", () => {
+    it("should redirect to enter-phone-number when 2 factor is required", async () => {
+      const fakeUserAuthService: AuthenticationServiceInterface = {
+        userExists: sandbox.fake(),
+        signUpUser: sandbox.fake.returns(USER_STATE.REQUIRES_TWO_FACTOR),
+      };
+
+      req.body.password = "password1";
+      req.session.user.email = "joe.bloggs@test.com";
+      req.session.user.id = "test.test.com";
+
+      await createPasswordPost(fakeUserAuthService)(
+        req as Request,
+        res as Response,
+        next
+      );
+
+      expect(res.redirect).to.have.calledWith("/enter-phone-number");
+    });
+
+    it("should redirect to account-created when 2 factor is not required", async () => {
+      const fakeUserAuthService: AuthenticationServiceInterface = {
+        userExists: sandbox.fake(),
+        signUpUser: sandbox.fake.returns(""),
+      };
+
+      req.body.password = "password1";
+      req.session.user.email = "joe.bloggs@test.com";
+      req.session.user.id = "test.test.com";
+
+      await createPasswordPost(fakeUserAuthService)(
+        req as Request,
+        res as Response,
+        next
+      );
+
+      expect(res.redirect).to.have.calledWith("/account-created");
+    });
+
+    it("should throw invalid session error when session not populated", async () => {
+      req.session = undefined;
+
+      await expect(
+        createPasswordPost(null)(req as Request, res as Response, next)
+      );
+
+      expect(next).to.have.been.calledOnce;
+    });
+
+    it("should call next with error when api throws error", async () => {
+      const fakeUserAuthService: AuthenticationServiceInterface = {
+        userExists: sandbox.fake(),
+        signUpUser: sandbox.fake.throws(Error("error")),
+      };
+
+      req.body.email = "test.test.com";
+      req.session.user.id = "test.test.com";
+
+      await createPasswordPost(fakeUserAuthService)(
+        req as Request,
+        res as Response,
+        next
+      );
+
+      expect(next).to.be.calledOnce;
+    });
+
+  });
+});
