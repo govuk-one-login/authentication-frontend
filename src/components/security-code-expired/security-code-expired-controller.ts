e4a915bb1811b551c3df5f6ad9bc7d90ffba3180
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-07-06 17:37:15 +0100

    GOVSI-429: remove post route for security-code-expired
    Temporarily redirect to enter-phone-number when clicking continue

--- src/components/security-code-expired/security-code-expired-controller.ts
+++ src/components/security-code-expired/security-code-expired-controller.ts
@@ -4,6 +4,3 @@ export function securityCodeExpiredGet(req: Request, res: Response): void {
   res.render("security-code-expired/index.njk");
 }
 
-export function securityCodeExpiredPost(req: Request, res: Response): void {
-  res.redirect("/enter-email");
-}
--- src/components/security-code-expired/security-code-expired-routes.ts
+++ src/components/security-code-expired/security-code-expired-routes.ts
@@ -3,7 +3,6 @@ import { PATH_NAMES } from "../../app.constants";
 import * as express from "express";
 import {
   securityCodeExpiredGet,
-  securityCodeExpiredPost,
 } from "./security-code-expired-controller";
 import { validateSessionMiddleware } from "../../middleware/session-middleware";
 
@@ -13,11 +12,6 @@ router.get(
   PATH_NAMES.SECURITY_CODE_EXPIRED,
   validateSessionMiddleware,
   securityCodeExpiredGet
-);
-router.post(
-  PATH_NAMES.SECURITY_CODE_EXPIRED,
-  validateSessionMiddleware,
-  securityCodeExpiredPost
 );
 
 export { router as securityCodeExpiredRouter };
