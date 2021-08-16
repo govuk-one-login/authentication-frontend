8ab5f5e9cfeabf2301771b922186b66a009ead2f
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-08-16 18:03:28 +0100

    GOVSI-281: updated content for 'share-info' page
    Page content has been updated in the Figma.
    By default no option will be selected.
    On clicking continue an error message will be displayed if no option is selected.

---
+++ src/components/share-info/tests/share-info-integration.test.ts
@@ -0,0 +1,145 @@
+import request from "supertest";
+import { describe } from "mocha";
+import { expect, sinon } from "../../../../test/utils/test-utils";
+import nock = require("nock");
+import * as cheerio from "cheerio";
+import decache from "decache";
+
+describe("Integration::share info", () => {
+  let sandbox: sinon.SinonSandbox;
+  let token: string | string[];
+  let cookies: string;
+  let app: any;
+  let baseApi: string;
+
+  before(() => {
+    decache("../../../app");
+    decache("../../../middleware/session-middleware");
+    const sessionMiddleware = require("../../../middleware/session-middleware");
+    sandbox = sinon.createSandbox();
+    sandbox
+      .stub(sessionMiddleware, "validateSessionMiddleware")
+      .callsFake(function (req: any, res: any, next: any): void {
+        res.locals.sessionId = "tDy103saszhcxbQq0-mjdzU854";
+        res.locals.clientSessionId = "csy103saszhcxbQq0-mjdzU854";
+        req.session.user = {
+          email: "test@test.com",
+        };
+        next();
+      });
+
+    app = require("../../../app").createApp();
+    baseApi = process.env.API_BASE_URL;
+
+    nock(baseApi)
+      .get("/client-info")
+      .once()
+      .reply(200, {
+        clientId: "client_test",
+        clientName: "client_test_name",
+        scopes: ["email", "phone"],
+      });
+
+    request(app)
+      .get("/share-info")
+      .end((err, res) => {
+        const $ = cheerio.load(res.text);
+        token = $("[name=_csrf]").val();
+        cookies = res.headers["set-cookie"];
+      });
+  });
+
+  beforeEach(() => {
+    nock.cleanAll();
+  });
+
+  after(() => {
+    sandbox.restore();
+    app = undefined;
+  });
+
+  it("should return share info page", (done) => {
+    nock(baseApi)
+      .get("/client-info")
+      .once()
+      .reply(200, {
+        clientId: "client_test",
+        clientName: "client_test_name",
+        scopes: ["email", "phone"],
+      });
+    request(app).get("/share-info").expect(200, done);
+  });
+
+  it("should return error when csrf not present", (done) => {
+    request(app)
+      .post("/share-info")
+      .type("form")
+      .send({
+        consentValue: "true",
+      })
+      .expect(500, done);
+  });
+
+  it("should return validation error when consentValue not selected", (done) => {
+    request(app)
+      .post("/share-info")
+      .type("form")
+      .set("Cookie", cookies)
+      .send({
+        _csrf: token,
+        consentValue: undefined,
+      })
+      .expect(function (res) {
+        const $ = cheerio.load(res.text);
+        expect($("#share-info-error").text()).to.contains(
+          "Select if you want to share your email address and phone number or not"
+        );
+      })
+      .expect(400, done);
+  });
+
+  it("should redirect to /auth-code page when consentValue valid", (done) => {
+    nock(baseApi)
+      .post("/update-profile")
+      .once()
+      .reply(200, {
+        sessionState: "ADDED_CONSENT",
+      });
+
+    request(app)
+      .post("/share-info")
+      .type("form")
+      .set("Cookie", cookies)
+      .send({
+        _csrf: token,
+        consentValue: "true",
+      })
+      .expect("Location", "/auth-code")
+      .expect(302, done);
+  });
+
+  it("should return internal server error when /update-profile API call response is 500", (done) => {
+    nock(baseApi)
+      .post("/update-profile")
+      .once()
+      .reply(500, {});
+
+    request(app)
+      .post("/share-info")
+      .type("form")
+      .set("Cookie", cookies)
+      .send({
+        _csrf: token,
+        consentValue: "true",
+      })
+      .expect(500, done);
+  });
+
+  it("should return internal server error when /client-info API call response is 500", (done) => {
+    nock(baseApi)
+      .get("/client-info")
+      .once()
+      .reply(500, {});
+    request(app).get("/share-info").expect(500, done);
+  });
+});
