00ea0474ec63db54e7b68f571f398d16ef57bb6d
Sandeep Nagra <san@initialspark.co.uk>
2021-10-11 13:08:06 +0100

    Handle logout errors via front end

--- src/components/signed-out/signed-out-controller.ts
+++ src/components/signed-out/signed-out-controller.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from "express";
+import { BadRequestError } from "../../utils/error";
 
 export function signedOutGet(req: Request, res: Response): void {
+  if (req.query && req.query.error_code) {
+    throw new BadRequestError(
+      req.query.error_description as string,
+      req.query.error_code as string
+    );
+  }
+
   if (req.cookies) {
     for (const cookieName of Object.keys(req.cookies)) {
       if (cookieName !== "lng" && cookieName !== "cookies_preferences_set") {
@@ -8,5 +16,6 @@ export function signedOutGet(req: Request, res: Response): void {
       }
     }
   }
+
   res.render("signed-out/index.njk");
 }
