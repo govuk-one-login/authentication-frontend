c6aed02cf6c8ffc048c79aab54c7c7dc5a9614ee
dannyhayes-gds <daniel.hayes@digital.cabinet-office.gov.uk>
2021-10-12 14:11:19 +0100

    Fix content changes and setup ACCOUNT_MANAGEMENT_URL env var

--- src/middleware/set-local-vars-middleware.ts
+++ src/middleware/set-local-vars-middleware.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from "express";
-import { getAnalyticsCookieDomain, getGtmId } from "../config";
+import {
+  getAccountManagementUrl,
+  getAnalyticsCookieDomain,
+  getGtmId,
+} from "../config";
 import { generateNonce } from "../utils/strings";
 
 export function setLocalVarsMiddleware(
@@ -10,5 +14,6 @@ export function setLocalVarsMiddleware(
   res.locals.gtmId = getGtmId();
   res.locals.scriptNonce = generateNonce();
   res.locals.analyticsCookieDomain = getAnalyticsCookieDomain();
+  res.locals.accountManagementUrl = getAccountManagementUrl();
   next();
 }
