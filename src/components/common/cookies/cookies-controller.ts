b71cdbe4877d2fa45204f2c74063e7d1bbcdcb9b
dannyhayes-gds <daniel.hayes@digital.cabinet-office.gov.uk>
2021-10-20 14:41:23 +0100

    GOVSI-973: Add redirect link to previous page for cookies page

--- src/components/common/cookies/cookies-controller.ts
+++ src/components/common/cookies/cookies-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 
 export function cookiesGet(req: Request, res: Response): void {
+  res.locals.backUrl = req.get("Referrer");
   res.render("common/cookies/index.njk");
 }
