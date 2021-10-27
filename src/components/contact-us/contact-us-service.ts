88325cf826a00c4caf5da52e3f2e6ba3027d5083
Sandeep Nagra <san@initialspark.co.uk>
2021-10-27 13:49:24 +0100

    Update zendesk payload to support when contact details are not provided

--- src/components/contact-us/contact-us-service.ts
+++ src/components/contact-us/contact-us-service.ts
@@ -9,18 +9,21 @@ export function contactUsService(
   const contactUsSubmitForm = async function (
     contactForm: ContactForm
   ): Promise<void> {
-    const payload = {
+    const payload: any = {
       ticket: {
         subject: contactForm.subject,
-        requester: {
-          email: contactForm.email ?? "",
-          name: contactForm.name ?? "",
-        },
         comment: { body: contactForm.comment },
         group_id: getZendeskGroupIdPublic(),
         tags: ["govuk_sign_in"],
       },
     };
+
+    if (contactForm.email && contactForm.name) {
+      payload.ticket.requester = {
+        email: contactForm.email,
+        name: contactForm.name,
+      };
+    }
 
     await zendeskClient.tickets.create(payload);
   };
