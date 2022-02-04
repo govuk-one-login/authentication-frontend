fefa1c95220e2c9978f9b113060f4df63f6918d9
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-02-04 14:52:15 +0000

    AUTH-1309 - Add relevant error messages to contact-us-questions
    - Check what the theme and subtheme is in the validator and apply the relevant error message
    - Make the welsh translation file consistent with the english file

--- src/components/contact-us/contact-us-questions-validation.ts
+++ src/components/contact-us/contact-us-questions-validation.ts
@@ -1,6 +1,7 @@
 import { body } from "express-validator";
 import { validateBodyMiddleware } from "../../middleware/form-validation-middleware";
 import { ValidationChainFunc } from "../../types";
+import { ZENDESK_THEMES } from "../../app.constants";
 
 export function validateContactUsQuestionsRequest(): ValidationChainFunc {
   return [
@@ -8,13 +9,22 @@ export function validateContactUsQuestionsRequest(): ValidationChainFunc {
       .optional()
       .notEmpty()
       .withMessage((value, { req }) => {
-        return req.t("error-message", { value });
+        return req.t(
+          getErrorMessageForIssueDescription(req.body.theme, req.body.subtheme),
+          { value }
+        );
       }),
     body("additionalDescription")
       .optional()
       .notEmpty()
       .withMessage((value, { req }) => {
-        return req.t("temp-error-message", { value });
+        return req.t(
+          getErrorMessageForAdditionalDescription(
+            req.body.theme,
+            req.body.subtheme
+          ),
+          { value }
+        );
       }),
     body("replyEmail")
       .if(body("feedbackContact").equals("true"))
@@ -43,4 +53,52 @@ export function validateContactUsQuestionsRequest(): ValidationChainFunc {
       }),
     validateBodyMiddleware("contact-us/questions/index.njk"),
   ];
+}
+
+export function getErrorMessageForIssueDescription(
+  theme: string,
+  subtheme: string
+): string {
+  if (theme === ZENDESK_THEMES.EMAIL_SUBSCRIPTIONS) {
+    return "pages.contactUsQuestions.emailSubscriptions.section1.errorMessage";
+  }
+  if (theme === ZENDESK_THEMES.SOMETHING_ELSE) {
+    return "pages.contactUsQuestions.anotherProblem.section1.errorMessage";
+  }
+  if (theme === ZENDESK_THEMES.SUGGESTIONS_FEEDBACK) {
+    return "pages.contactUsQuestions.suggestionOrFeedback.section1.errorMessage";
+  }
+  if (subtheme === ZENDESK_THEMES.ACCOUNT_NOT_FOUND) {
+    return "pages.contactUsQuestions.accountNotFound.section1.errorMessage";
+  }
+  if (subtheme === ZENDESK_THEMES.TECHNICAL_ERROR) {
+    return "pages.contactUsQuestions.technicalError.section1.errorMessage";
+  }
+  if (
+    theme === ZENDESK_THEMES.SIGNING_IN &&
+    subtheme === ZENDESK_THEMES.SOMETHING_ELSE
+  ) {
+    return "pages.contactUsQuestions.signignInProblem.section1.errorMessage";
+  }
+  if (
+    theme === ZENDESK_THEMES.ACCOUNT_CREATION &&
+    subtheme === ZENDESK_THEMES.SOMETHING_ELSE
+  ) {
+    return "pages.contactUsQuestions.accountCreationProblem.section1.errorMessage";
+  }
+}
+
+export function getErrorMessageForAdditionalDescription(
+  theme: string,
+  subtheme: string
+): string {
+  if (theme === ZENDESK_THEMES.EMAIL_SUBSCRIPTIONS) {
+    return "pages.contactUsQuestions.emailSubscriptions.section1.errorMessage";
+  }
+  if (theme === ZENDESK_THEMES.SOMETHING_ELSE) {
+    return "pages.contactUsQuestions.anotherProblem.section2.errorMessage";
+  }
+  if (subtheme === ZENDESK_THEMES.TECHNICAL_ERROR) {
+    return "pages.contactUsQuestions.technicalError.section2.errorMessage";
+  }
 }
