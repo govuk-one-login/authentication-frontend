bab85d8a66aeb37675d616755c5c430743b207fc
dbes-gds <38218115+dbes-gds@users.noreply.github.com>
2022-04-05 13:25:46 +0100

    Revert "Revert "AUTH-1477: Reset password with code""

--- src/components/reset-password/tests/reset-password-controller.test.ts
+++ src/components/reset-password/tests/reset-password-controller.test.ts
@@ -7,6 +7,7 @@ import { Request, Response } from "express";
 import {
   resetPasswordGet,
   resetPasswordPost,
+  resetPasswordRequestGet,
 } from "../reset-password-controller";
 import { ResetPasswordServiceInterface } from "../types";
 import { PATH_NAMES } from "../../../app.constants";
@@ -17,24 +18,40 @@ import {
   ResponseOutput,
 } from "mock-req-res";
 
-describe("reset password controller", () => {
+describe("reset password controller (in 6 digit code flow)", () => {
   let req: RequestOutput;
   let res: ResponseOutput;
 
   beforeEach(() => {
-    req = mockRequest();
+    req = mockRequest({
+      path: PATH_NAMES.RESET_PASSWORD,
+      session: { client: {}, user: {} },
+      log: { info: sinon.fake() },
+      t: sinon.fake(),
+      i18n: { language: "en" },
+    });
     res = mockResponse();
+    res.locals.sessionId = "s-123456-djjad";
   });
 
   afterEach(() => {
     sinon.restore();
   });
 
+  describe("resetPasswordRequestGet", () => {
+    it("should redirect to /reset-password-check-mail when reset password requested", () => {
+
+      req.path = PATH_NAMES.RESET_PASSWORD_REQUEST;
+
+      resetPasswordRequestGet(req as Request, res as Response);
+
+      expect(res.redirect).to.have.calledWith(PATH_NAMES.RESET_PASSWORD_CHECK_EMAIL);
+    });
+  });
+
+
   describe("resetPasswordGet", () => {
     it("should render change password page", () => {
-      req.query.code =
-        "asdkki8ddas.1758350212000.some-session-id.some-persistent-session-id";
-
       resetPasswordGet(req as Request, res as Response);
 
       expect(res.render).to.have.calledWith("reset-password/index.njk");
@@ -42,75 +59,23 @@ describe("reset password controller", () => {
   });
 
   describe("resetPasswordPost", () => {
-    it("should redirect to /reset-password-confirmation page", async () => {
+    it("should redirect to /auth-code when password updated", async () => {
       const fakeService: ResetPasswordServiceInterface = {
         updatePassword: sinon.fake.returns({ success: true }),
       };
 
+      req.session.user = {
+        email: "joe.bloggs@test.com",
+      };
       req.body.password = "Password1";
-      req.body.code =
-        "asdkki8ddas.1758350212000.some-session-id.some-persistent-session-id";
 
       await resetPasswordPost(fakeService)(req as Request, res as Response);
 
       expect(fakeService.updatePassword).to.have.been.calledOnce;
       expect(res.redirect).to.have.calledWith(
-        PATH_NAMES.RESET_PASSWORD_CONFIRMATION
-      );
-    });
-  });
-
-  describe("resetPasswordPost", () => {
-    it("should redirect to /reset-password-expired-link when timestamp has been tampered with", async () => {
-      const fakeService: ResetPasswordServiceInterface = {
-        updatePassword: sinon.fake.returns({ success: true }),
-      };
-
-      req.body.password = "Password1";
-      req.body.code =
-        "asdkki8ddas.234.some-session-id.some-persistent-session-id";
-
-      await resetPasswordPost(fakeService)(req as Request, res as Response);
-
-      expect(fakeService.updatePassword).to.have.been.not.calledOnce;
-      expect(res.redirect).to.have.calledWith(
-        PATH_NAMES.RESET_PASSWORD_EXPIRED_LINK
-      );
-    });
-  });
-  describe("resetPasswordPost", () => {
-    it("should redirect to /reset-password-expired-link when code is missing", async () => {
-      const fakeService: ResetPasswordServiceInterface = {
-        updatePassword: sinon.fake.returns({ success: true }),
-      };
-
-      req.body.password = "Password1";
-      req.body.code =
-        "1758350212000.some-session-id.some-persistent-session-id";
-
-      await resetPasswordPost(fakeService)(req as Request, res as Response);
-
-      expect(fakeService.updatePassword).to.have.been.not.calledOnce;
-      expect(res.redirect).to.have.calledWith(
-        PATH_NAMES.RESET_PASSWORD_EXPIRED_LINK
-      );
-    });
-  });
-  describe("resetPasswordPost", () => {
-    it("should redirect to /reset-password-expired-link when session-id is missing", async () => {
-      const fakeService: ResetPasswordServiceInterface = {
-        updatePassword: sinon.fake.returns({ success: true }),
-      };
-
-      req.body.password = "Password1";
-      req.body.code = "asdkki8ddas.1758350212000.some-persistent-session-id";
-
-      await resetPasswordPost(fakeService)(req as Request, res as Response);
-
-      expect(fakeService.updatePassword).to.have.been.not.calledOnce;
-      expect(res.redirect).to.have.calledWith(
-        PATH_NAMES.RESET_PASSWORD_EXPIRED_LINK
+        PATH_NAMES.AUTH_CODE
       );
     });
   });
 });
+
--- src/components/reset-password/types.ts
+++ src/components/reset-password/types.ts
@@ -3,7 +3,6 @@ import { ApiResponseResult, DefaultApiResponse } from "../../types";
 export interface ResetPasswordServiceInterface {
   updatePassword: (
     newPassword: string,
-    code: string,
     sourceIp: string,
     sessionId: string,
     persistentSessionId: string
