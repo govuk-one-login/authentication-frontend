0edcf8c12362ad9a30b1c829e9d2069775971028
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2022-07-27 15:17:47 +0100

    AUT-582: Add an external route to the http 500 error page.
    So that the doc-app-callback lambda can redirect to this error page in the event of an internal server error.

---
+++ src/components/common/errors/error-controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+import { HTTP_STATUS_CODES } from "../../../app.constants";
+
+export function errorPageGet(req: Request, res: Response): void {
+  res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR);
+  res.render("common/errors/500.njk");
+}
