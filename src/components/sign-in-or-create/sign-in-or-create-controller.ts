7b658257b1c511c7252840340a37df92d3752c11
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-08-03 11:58:10 +0100

    AUT-601 - Change content on create screen for auth apps
    - State that a user can receive a security code via an auth app when auth apps are enabled.

--- src/components/sign-in-or-create/sign-in-or-create-controller.ts
+++ src/components/sign-in-or-create/sign-in-or-create-controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { getNextPathAndUpdateJourney } from "../common/constants";
 import { USER_JOURNEY_EVENTS } from "../common/state-machine/state-machine";
+import { supportMFAOptions } from "../../config";
 
 export function signInOrCreateGet(req: Request, res: Response): void {
   res.render("sign-in-or-create/index.njk", {
     serviceType: req.session.client.serviceType,
+    supportMFAOptions: supportMFAOptions() ? true : null,
   });
 }
 
