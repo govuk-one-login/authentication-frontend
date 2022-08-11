f1a8a8c7898e27211305cdd9e7eebee3dd7ed15c
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-08-11 15:12:00 +0100

    AUT-611 - Change content for IPNs

--- src/components/sign-in-or-create/sign-in-or-create-controller.ts
+++ src/components/sign-in-or-create/sign-in-or-create-controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import { getNextPathAndUpdateJourney } from "../common/constants";
 import { USER_JOURNEY_EVENTS } from "../common/state-machine/state-machine";
-import { supportMFAOptions } from "../../config";
+import { supportInternationalNumbers, supportMFAOptions } from "../../config";
 
 export function signInOrCreateGet(req: Request, res: Response): void {
   res.render("sign-in-or-create/index.njk", {
     serviceType: req.session.client.serviceType,
     supportMFAOptions: supportMFAOptions() ? true : null,
+    supportInternationalNumbers: supportInternationalNumbers() ? true : null,
   });
 }
 
