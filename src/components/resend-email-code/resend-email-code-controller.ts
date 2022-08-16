0f27330f898cd48f6ff5495e0057d08a856dbf78
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-08-16 11:13:09 +0100

    AUT-555 - Send email code again
    - Create new component to send the email code again. Make this a different component to the send mfa code again to ensure it can only be accessed via the relevant state transition.
    - When the user clicks `Get security code` on the resend-email-code component they will be redirected to /check-email

---
+++ src/components/resend-email-code/resend-email-code-controller.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import { ExpressRouteFunc } from "../../types";
+import { NOTIFICATION_TYPE } from "../../app.constants";
+import {
+  getErrorPathByCode,
+  getNextPathAndUpdateJourney,
+} from "../common/constants";
+import { BadRequestError } from "../../utils/error";
+import { USER_JOURNEY_EVENTS } from "../common/state-machine/state-machine";
+import { SendNotificationServiceInterface } from "../common/send-notification/types";
+import { sendNotificationService } from "../common/send-notification/send-notification-service";
+
+export function resendEmailCodeGet(req: Request, res: Response): void {
+  res.render("resend-email-code/index.njk", {
+    emailAddress: req.session.user.email,
+  });
+}
+
+export function resendEmailCodePost(
+  notificationService: SendNotificationServiceInterface = sendNotificationService()
+): ExpressRouteFunc {
+  return async function (req: Request, res: Response) {
+    const email = req.session.user.email.toLowerCase();
+    const { sessionId, clientSessionId, persistentSessionId } = res.locals;
+
+    const sendNotificationResponse = await notificationService.sendNotification(
+      sessionId,
+      clientSessionId,
+      email,
+      NOTIFICATION_TYPE.VERIFY_EMAIL,
+      req.ip,
+      persistentSessionId
+    );
+
+    if (!sendNotificationResponse.success) {
+      const path = getErrorPathByCode(sendNotificationResponse.data.code);
+
+      if (path) {
+        return res.redirect(path);
+      }
+
+      throw new BadRequestError(
+        sendNotificationResponse.data.message,
+        sendNotificationResponse.data.code
+      );
+    }
+
+    return res.redirect(
+      getNextPathAndUpdateJourney(
+        req,
+        req.path,
+        USER_JOURNEY_EVENTS.SEND_EMAIL_CODE,
+        null,
+        sessionId
+      )
+    );
+  };
+}
