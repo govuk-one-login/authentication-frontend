84d789531d768353631a88a66cdbdc1dda06c70f
ayoshebby <ayo.sebiotimo@digital.cabinet-office.gov.uk>
2023-02-07 11:20:47 +0000

    AUT-995: Review WIP

--- src/components/enter-mfa/enter-mfa-controller.ts
+++ src/components/enter-mfa/enter-mfa-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { NOTIFICATION_TYPE } from "../../app.constants";
+import {MFA_METHOD_TYPE, NOTIFICATION_TYPE} from "../../app.constants";
 import { VerifyCodeInterface } from "../common/verify-code/types";
 import { codeService } from "../common/verify-code/verify-code-service";
 import { verifyCodePost } from "../common/verify-code/verify-code-controller";
@@ -11,6 +11,7 @@ const TEMPLATE_NAME = "enter-mfa/index.njk";
 export function enterMfaGet(req: Request, res: Response): void {
   res.render(TEMPLATE_NAME, {
     phoneNumber: req.session.user.phoneNumber,
+    isAuthApp: req.session.user.mfaMethodType === MFA_METHOD_TYPE.AUTH_APP
   });
 }
 
