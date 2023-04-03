71c5dcb489d8bf995cc322b808db42fbf403421c
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2023-04-03 11:37:09 +0100

    Add hosted zone, to decouple from main DNS configuration.

--- ci/terraform/dns.tf
+++ ci/terraform/dns.tf
@@ -1,5 +1,5 @@
 data "terraform_remote_state" "dns" {
-  count = var.account_management_fqdn == null && var.oidc_api_fqdn == null ? 1 : 0
+  count = var.zone_id == null ? 1 : 0
 
   backend = "s3"
   config = {
@@ -11,10 +11,10 @@ data "terraform_remote_state" "dns" {
 }
 
 locals {
-  account_management_fqdn = var.account_management_fqdn == null ? lookup(data.terraform_remote_state.dns[0].outputs, "${var.environment}_account_management_url", "") : var.account_management_fqdn
-  frontend_fqdn           = var.frontend_fqdn == null ? lookup(data.terraform_remote_state.dns[0].outputs, "${var.environment}_frontend_url", "") : var.frontend_fqdn
-  frontend_api_fqdn       = var.frontend_api_fqdn == null ? lookup(data.terraform_remote_state.dns[0].outputs, "${var.environment}_api_frontend_url", "") : var.frontend_api_fqdn
-  oidc_api_fqdn           = var.oidc_api_fqdn == null ? lookup(data.terraform_remote_state.dns[0].outputs, "${var.environment}_api_url", "") : var.oidc_api_fqdn
-  service_domain          = var.service_domain == null ? lookup(data.terraform_remote_state.dns[0].outputs, "${var.environment}_service_domain", "") : var.service_domain
+  service_domain          = var.service_domain == null ? "${var.environment}.account.gov.uk" : var.service_domain
+  account_management_fqdn = local.service_domain
+  frontend_fqdn           = "signin.${local.service_domain}"
+  frontend_api_fqdn       = "auth.${local.service_domain}"
+  oidc_api_fqdn           = "oidc.${local.service_domain}"
   zone_id                 = var.zone_id == null ? lookup(data.terraform_remote_state.dns[0].outputs, "${var.environment}_zone_id", "") : var.zone_id
 }
--- ci/terraform/route53.tf
+++ ci/terraform/route53.tf
@@ -1,7 +1,23 @@
+resource "aws_route53_zone" "zone" {
+  name = local.frontend_fqdn
+}
+
 resource "aws_route53_record" "frontend" {
   name    = local.frontend_fqdn
   type    = "A"
   zone_id = local.zone_id
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_lb.frontend_alb.dns_name
+    zone_id                = aws_lb.frontend_alb.zone_id
+  }
+}
+
+resource "aws_route53_record" "frontend_record" {
+  name    = local.frontend_fqdn
+  type    = "A"
+  zone_id = aws_route53_record.frontend.zone_id
 
   alias {
     evaluate_target_health = false
@@ -35,7 +51,29 @@ resource "aws_route53_record" "frontend_alb_certificate_validation" {
   zone_id         = local.zone_id
 }
 
+resource "aws_route53_record" "frontend_validation_record" {
+
+  for_each = {
+    for dvo in aws_acm_certificate.frontend_alb_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.zone.zone_id
+}
+
 resource "aws_acm_certificate_validation" "frontend_acm_alb_certificate_validation" {
   certificate_arn         = aws_acm_certificate.frontend_alb_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.frontend_alb_certificate_validation : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.frontend_validation_record : record.fqdn]
+}
+
+output "signin_nameservers" {
+  value = aws_route53_zone.zone.name_servers
 }
