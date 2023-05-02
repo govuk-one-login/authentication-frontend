417d67859a2c2f141a851a8290977e950d0311de
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2023-05-02 11:44:13 +0100

    AUT-1080: remove duplicated component

--- src/components/change-security-codes-confirmation/change-security-codes-confirmation-controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-export function changeSecurityCodesConfirmationGet(
-  _req: Request,
-  res: Response
-): void {
-  res.render("change-security-codes-confirmation/index.njk");
-}
