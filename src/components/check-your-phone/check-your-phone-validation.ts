914f7ef2c51a446385a8f7197f89ae31f6917a9a
Dominic Taylor <dominic.taylor@digital.cabinet-office.gov.uk>
2023-07-17 14:58:33 +0100

    AUT-1426: Fix code resend link SMS account recovery
    - When a user submitted an incorrect or invalid code in account recovery, choosing SMS, they would lost a 'local' in the validation process
    - This happened through 2 different mechanisms - one for invalid code e.g. "1a3" vs simply incorrect (but valid format) e.g. "111111"
    - Each mechanism has been fixed separately:
    - For invalid codes, the resend URL will now be passed as a post validation local via the validator
    - For incorrect codes, it will also be passed as a post validation local but via a conditional block in the POST handler/controller
    - i.e. these two scenarios required a separate fix

--- src/components/check-your-phone/check-your-phone-validation.ts
+++ src/components/check-your-phone/check-your-phone-validation.ts
@@ -1,6 +1,8 @@
 import { validateBodyMiddleware } from "../../middleware/form-validation-middleware";
 import { ValidationChainFunc } from "../../types";
 import { validateCode } from "../common/verify-code/verify-code-validation";
+import { pathWithQueryParam } from "../common/constants";
+import { PATH_NAMES } from "../../app.constants";
 
 export function validateSmsCodeRequest(): ValidationChainFunc {
   return [
@@ -10,6 +12,17 @@ export function validateSmsCodeRequest(): ValidationChainFunc {
       minLengthKey: "pages.checkYourPhone.code.validationError.minLength",
       numbersOnlyKey: "pages.checkYourPhone.code.validationError.invalidFormat",
     }),
-    validateBodyMiddleware("check-your-phone/index.njk"),
+    validateBodyMiddleware("check-your-phone/index.njk", postValidationLocals),
   ];
 }
+
+const postValidationLocals = function locals(): Record<string, unknown> {
+  const resendCodeLinkAsPostValidationLocal = pathWithQueryParam(
+    PATH_NAMES.RESEND_MFA_CODE_ACCOUNT_CREATION,
+    "isResendCodeRequest",
+    "true"
+  );
+  return {
+    resendCodeLink: resendCodeLinkAsPostValidationLocal,
+  };
+};
