7f03aaa9ab3b7f0c0d2a7577f9eb8317eebf8144
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-09-11 10:48:58 +0100

    BAU: Rename test file and fix tests
    These tests weren't being run as part of CI since the test file was did not have the .test.ts file extension. A refactor to the generateQRCodeValue actually broke these tests, but this wasn't picked up due to the file name.

--- test/unit/utils/mfa.test.ts
+++ test/unit/utils/mfa.test.ts
@@ -12,11 +12,14 @@ describe("mfa", () => {
 
   describe("generateQRCodeValue", () => {
     it("should generate QR code value with correct inputs", () => {
-      const expected =
-        "otpauth://totp/GOV.UK%20SignIn:test%40test.com?secret=testsecret&period=30&digits=6&algorithm=SHA1&issuer=GOV.UK%20SignIn";
-      expect(generateQRCodeValue("testsecret", "test@test.com")).to.equal(
-        expected
-      );
+      const secret = "testSecret";
+      const email = "test@test.com";
+      const urlEncodedEmail = "test%40test.com";
+      const issuerName = "GOV.UK SignIn";
+      const urlEncodedIssuerName = "GOV.UK%20SignIn%20-%20local";
+
+      const expected = `otpauth://totp/${urlEncodedIssuerName}:${urlEncodedEmail}?secret=${secret}&period=30&digits=6&algorithm=SHA1&issuer=${urlEncodedIssuerName}`;
+      expect(generateQRCodeValue(secret, email, issuerName)).to.equal(expected);
     });
   });
 });
