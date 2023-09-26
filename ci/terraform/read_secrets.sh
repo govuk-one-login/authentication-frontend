c7b0e9e5fedc8b2a0dede0a9755aa7dea017e62f
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2023-09-26 14:00:19 +0100

    AUT-1720: don't try to export invalid tfvars
    Add a check to `read_secrets.sh` to ensure that the secret name is a
    valid environment variable name (eg. a not secret name with a hyphen in
    it) before attempting to export the variable.
    
    If the name is not a valid `bash` variable name, the script errors and
    exits.
    
    Previously, the script would error and exit anyway, but the error was
    not particularly helpful (`'some-variable=something': not a valid
    identifier`).

--- ci/terraform/read_secrets.sh
+++ ci/terraform/read_secrets.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] || {
+  echo "Error: Script must be sourced, not executed"
+  exit 1
+}
+
 ENVIRONMENT="${1}"
 
 if [ "$ENVIRONMENT" = "dev" ]; then
@@ -13,12 +18,16 @@ secrets="$(
     jq -r '.SecretList[]|[.ARN,(.Name|split("/")|last)]|@tsv'
 )"
 
+if [ -z "${secrets}" ]; then
+  printf '!! ERROR: No secrets found for environment %s. Exiting.\n' "${ENVIRONMENT}" >&2
+  exit 1
+fi
+
 while IFS=$'\t' read -r arn name; do
+  if [[ ! ${name} =~ ^[a-zA-Z0-9_]+$ ]]; then
+    printf '!! Invalid secret name: %s. Secret name must match /^[a-zA-Z0-9_]+/. Exiting' "${name}" >&2
+    exit 1
+  fi
   value=$(aws secretsmanager get-secret-value --secret-id "${arn}" | jq -r '.SecretString')
   export "TF_VAR_${name}"="${value}"
 done <<<"${secrets}"
-
-if [ "$TF_VAR_basic_auth_password" = "none" ]; then
-  export TF_VAR_basic_auth_username=""
-  export TF_VAR_basic_auth_password=""
-fi
