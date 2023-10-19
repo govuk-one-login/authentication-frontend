b548ce5c67921ff26c4850d3720af1ba04cab422
melvabout <andrew.melvin@digital.cabinet-office.gov.uk>
2023-10-19 16:52:08 +0100

    Correct the suffix put the prefix back into the secrets

--- ci/terraform/dynatrace.tf
+++ ci/terraform/dynatrace.tf
@@ -1,5 +1,5 @@
 data "aws_secretsmanager_secret" "dynatrace_secrets" {
-  arn = var.environment == "production" ? "DynatraceProductionVariables-2i9U2A" : ":DynatraceNonProductionVariables-XuMvaO"
+  arn = var.environment == "production" ? "arn:aws:secretsmanager:eu-west-2:216552277552:secret:DynatraceProductionVariables" : "arn:aws:secretsmanager:eu-west-2:216552277552:secret:DynatraceNonProductionVariables"
 }
 
 data "aws_secretsmanager_secret_version" "dynatrace_secrets" {
