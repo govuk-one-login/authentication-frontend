5707d8be69e2412a46d5fa7e06dcc18eca02c6fb
melvabout <andrew.melvin@digital.cabinet-office.gov.uk>
2023-10-19 16:08:52 +0100

    Remove the prefix from the cross account secrets

--- ci/terraform/dynatrace.tf
+++ ci/terraform/dynatrace.tf
@@ -1,5 +1,5 @@
 data "aws_secretsmanager_secret" "dynatrace_secrets" {
-  arn = var.environment == "production" ? "arn:aws:secretsmanager:eu-west-2:216552277552:secret:DynatraceProductionVariables-2i9U2A" : "arn:aws:secretsmanager:eu-west-2:216552277552:secret:DynatraceNonProductionVariables-XuMvaO"
+  arn = var.environment == "production" ? "DynatraceProductionVariables-2i9U2A" : ":DynatraceNonProductionVariables-XuMvaO"
 }
 
 data "aws_secretsmanager_secret_version" "dynatrace_secrets" {
