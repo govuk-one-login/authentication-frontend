9e6ede90c31babd30e55c27363d81941d314837b
Aidan Comer <alhcomer@hotmail.co.uk>
2024-01-10 13:45:57 +0000

    AUT-1841: Fixes to allow password reset journey following account interventions

--- src/components/account-intervention/password-reset-required/password-reset-required-controller.ts
+++ src/components/account-intervention/password-reset-required/password-reset-required-controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
+import { PATH_NAMES } from "../../../app.constants";
 
 export function passwordResetRequiredGet(req: Request, res: Response): void {
-  res.render("account-intervention/password-reset-required/index.njk");
+  res.render("account-intervention/password-reset-required/index.njk", {
+    resetPasswordCheckEmailURL: PATH_NAMES.RESET_PASSWORD_CHECK_EMAIL,
+  });
 }
