8bb4969377d017f6d9c8fa244ae1beb582bc4ebb
GTVJ <gwyn.jones@digital.cabinet-office.gov.uk>
2024-02-08 13:45:15 +0000

    AUT-1339: Replace entry form and add first user form
    This commit:
    
    1. Deletes existing "A problem proving you identity" user form and
       replaces this with a gateway form providing access to the new form
       section
    2. Creates the new "You had a problem answering security questions" form
    
    This includes the Welsh translations and validation for both forms.

--- src/components/contact-us/contact-us-controller.ts
+++ src/components/contact-us/contact-us-controller.ts
@@ -63,6 +63,8 @@ const themeToPageTitle = {
     "pages.contactUsQuestions.provingIdentityFaceToFaceTechnicalProblem.title",
   [CONTACT_US_THEMES.PROVING_IDENTITY_FACE_TO_FACE_ANOTHER_PROBLEM]:
     "pages.contactUsQuestions.provingIdentityFaceToFaceSomethingElse.title",
+  [CONTACT_US_THEMES.PROVING_IDENTITY_PROBLEM_ANSWERING_SECURITY_QUESTIONS]:
+    "pages.contactUsQuestions.provingIdentityProblemAnsweringSecurityQuestions.title",
 };
 
 const somethingElseSubThemeToPageTitle = {
@@ -285,8 +287,25 @@ export function getPreferredLanguage(languageCode: string): string {
   return "Language code not set";
 }
 
+export function getNextUrlBasedOnTheme(theme: string): string {
+  let url: string = PATH_NAMES.CONTACT_US_QUESTIONS;
+
+  if (
+    [
+      CONTACT_US_THEMES.ACCOUNT_CREATION,
+      CONTACT_US_THEMES.SIGNING_IN,
+      CONTACT_US_THEMES.ID_CHECK_APP,
+      CONTACT_US_THEMES.PROVING_IDENTITY_FACE_TO_FACE,
+      CONTACT_US_THEMES.PROVING_IDENTITY,
+    ].includes(theme)
+  ) {
+    url = PATH_NAMES.CONTACT_US_FURTHER_INFORMATION;
+  }
+
+  return url;
+}
+
 export function contactUsFormPost(req: Request, res: Response): void {
-  let url = PATH_NAMES.CONTACT_US_QUESTIONS;
   const queryParams = new URLSearchParams({
     theme: req.body.theme,
     referer: validateReferer(req.body.referer, serviceDomain),
@@ -302,17 +321,9 @@ export function contactUsFormPost(req: Request, res: Response): void {
     );
   }
 
-  if (
-    [
-      CONTACT_US_THEMES.ACCOUNT_CREATION,
-      CONTACT_US_THEMES.SIGNING_IN,
-      CONTACT_US_THEMES.ID_CHECK_APP,
-      CONTACT_US_THEMES.PROVING_IDENTITY_FACE_TO_FACE,
-    ].includes(req.body.theme)
-  ) {
-    url = PATH_NAMES.CONTACT_US_FURTHER_INFORMATION;
-  }
-  res.redirect(url + "?" + queryParams.toString());
+  res.redirect(
+    getNextUrlBasedOnTheme(req.body.theme) + "?" + queryParams.toString()
+  );
 }
 
 export function furtherInformationGet(req: Request, res: Response): void {
@@ -776,6 +787,12 @@ export function getQuestionsFromFormTypeForMessageBody(
         { lng: "en" }
       ),
     },
+    provingIdentityProblemAnsweringSecurityQuestions: {
+      issueDescription: req.t(
+        "pages.contactUsQuestions.provingIdentityProblemAnsweringSecurityQuestions.section1.label",
+        { lng: "en" }
+      ),
+    },
   };
 
   return formTypeToQuestions[formType];
@@ -928,6 +945,13 @@ export function getQuestionFromThemes(
     ),
   };
 
+  const provingIdentitySubthemeToQuestion: { [key: string]: any } = {
+    proving_identity_problem_answering_security_questions: req.t(
+      "pages.contactUsQuestions.provingIdentityProblemAnsweringSecurityQuestions.title",
+      { lng: "en" }
+    ),
+  };
+
   const themeQuestion = themesToQuestions[theme];
   let subthemeQuestion;
   if (subtheme) {
@@ -942,6 +966,9 @@ export function getQuestionFromThemes(
     }
     if (theme == CONTACT_US_THEMES.PROVING_IDENTITY_FACE_TO_FACE) {
       subthemeQuestion = provingIdentityFaceToFaceSubthemeToQuestion[subtheme];
+    }
+    if (theme == CONTACT_US_THEMES.PROVING_IDENTITY) {
+      subthemeQuestion = provingIdentitySubthemeToQuestion[subtheme];
     }
   }
   return {
