c3f0818f63988693af236f2244888d877c018262
mark-cab <144702012+mark-cab@users.noreply.github.com>
2024-02-28 10:32:28 +0000

    copy update for inline incorrect password error (#1378)

--- src/components/enter-password/enter-password-controller.ts
+++ src/components/enter-password/enter-password-controller.ts
@@ -23,11 +23,15 @@ import { support2FABeforePasswordReset, support2hrLockout } from "../../config";
 import { getJourneyTypeFromUserSession } from "../common/journey/journey";
 
 const ENTER_PASSWORD_TEMPLATE = "enter-password/index.njk";
+const ENTER_PASSWORD_VALIDATION_KEY_OLD =
+  "pages.enterPassword.password.validationError.incorrectPassword_old";
 const ENTER_PASSWORD_VALIDATION_KEY =
   "pages.enterPassword.password.validationError.incorrectPassword";
 
 const ENTER_PASSWORD_ACCOUNT_EXISTS_TEMPLATE =
   "enter-password/index-account-exists.njk";
+const ENTER_PASSWORD_ACCOUNT_EXISTS_VALIDATION_KEY_OLD =
+  "pages.enterPasswordAccountExists.password.validationError.incorrectPassword_old";
 const ENTER_PASSWORD_ACCOUNT_EXISTS_VALIDATION_KEY =
   "pages.enterPasswordAccountExists.password.validationError.incorrectPassword";
 
@@ -114,15 +118,21 @@ export function enterPasswordPost(
       ) {
         return res.redirect(getErrorPathByCode(userLogin.data.code));
       }
-
-      const error = formatValidationError(
-        "password",
-        req.t(
-          fromAccountExists
-            ? ENTER_PASSWORD_ACCOUNT_EXISTS_VALIDATION_KEY
-            : ENTER_PASSWORD_VALIDATION_KEY
-        )
-      );
+      let validationKey;
+      if (support2hrLockout()) {
+        if (fromAccountExists) {
+          validationKey = ENTER_PASSWORD_ACCOUNT_EXISTS_VALIDATION_KEY;
+        } else {
+          validationKey = ENTER_PASSWORD_VALIDATION_KEY;
+        }
+      } else {
+        if (fromAccountExists) {
+          validationKey = ENTER_PASSWORD_ACCOUNT_EXISTS_VALIDATION_KEY_OLD;
+        } else {
+          validationKey = ENTER_PASSWORD_VALIDATION_KEY_OLD;
+        }
+      }
+      const error = formatValidationError("password", req.t(validationKey));
 
       return renderBadRequest(
         res,
