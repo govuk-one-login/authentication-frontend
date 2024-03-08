b04a4ec86d273e939424ce484f1c20bc0e1fbc50
David Walton <github@walton.io>
2024-03-08 11:58:11 +0000

    PLAT-3823 - Added cloudfront dataSource, added origin FQDN local

--- ci/terraform/dns.tf
+++ ci/terraform/dns.tf
@@ -8,11 +8,16 @@ locals {
 
   account_management_fqdn = var.environment == "production" ? "home.account.gov.uk" : "home.${var.environment}.account.gov.uk"
 
-  frontend_fqdn     = "signin.${local.service_domain}"
-  frontend_api_fqdn = "auth.${local.service_domain}"
-  oidc_api_fqdn     = "oidc.${local.service_domain}"
+  frontend_fqdn           = "signin.${local.service_domain}"
+  frontend_api_fqdn       = "auth.${local.service_domain}"
+  frontend_origin_fqdn    = "origin.signin.${local.service_domain}"
+  oidc_api_fqdn           = "oidc.${local.service_domain}"
 }
 
 data "aws_route53_zone" "service_domain" {
   name = local.service_domain
+}
+
+data "aws_cloudfront_distribution" "frontend" {
+  id = var.cloudfront_distribution_id
 }
