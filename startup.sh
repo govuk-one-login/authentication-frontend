e5e22f6dff90057bd57b017e9be7da1a932fe7e2
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2024-03-12 16:57:11 +0000

    Revert "Merge pull request #1433 from govuk-one-login/BAU/startup-aws-profile"
    This reverts commit 65a4231a6a0d926a77f9ce538a5b1dc1f8e500da, reversing
    changes made to 2f238925b372820445683a5ab4995c99f86dbffb.

--- startup.sh
+++ startup.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-set -e
+set -eu
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 CLEAN=0
 LOCAL=0
-
-AWS_PROFILE=gds-di-development-admin
 
 function usage() {
   local error_message="${1}"
@@ -14,7 +12,7 @@ function usage() {
   if [ -n "${error_message}" ]; then
     echo "Error: ${error_message}" >&2
   fi
-  echo "Usage: startup.sh [-c] [-l] [AWS_PROFILE]" >&2
+  echo "Usage: startup.sh [-c] [-l]" >&2
   echo "  -c: Clean dist and node_modules" >&2
   echo "  -l: Start frontend natively (not in docker)" >&2
   echo "  AWS_PROFILE: Optional name of aws profile to connect to the backend [di-auth-development-admin|di-auth-development-readonly] or as you have configured" >&2
@@ -54,7 +52,8 @@ docker-compose --log-level ERROR down
 
 test -f .env || usage "Missing .env file"
 
-export "$(grep -v '^#' .env | xargs)"
+# shellcheck source=/dev/null
+set -o allexport && source .env && set +o allexport
 
 # shellcheck source=./scripts/export_aws_creds.sh
 AWS_PROFILE=${AWS_PROFILE} source "${DIR}/scripts/export_aws_creds.sh"
