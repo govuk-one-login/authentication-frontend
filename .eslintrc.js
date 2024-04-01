e87429b224ae633e54f23ba0e8b79d96747b96e9
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2024-04-01 17:46:35 +0100

    BAU: Add eslint plugin and rule to prevent commiting 'it.only' tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint","no-only-tests"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
@@ -25,5 +25,6 @@ module.exports = {
       "error",
       { blankLine: "any", prev: "*", next: "*" },
     ],
+    "no-only-tests/no-only-tests": "error",
   },
 };
