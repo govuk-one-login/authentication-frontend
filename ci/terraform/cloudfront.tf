11d87415f6c66541e31969b511a7c546a3d4ed5b
Pawankumar Kushwaha <pawankumar.kushwaha@digital.cabinet-office.gov.uk>
2024-05-01 12:37:48 +0100

    fixed terraform format

--- ci/terraform/cloudfront.tf
+++ ci/terraform/cloudfront.tf
@@ -20,19 +20,19 @@ resource "aws_cloudformation_stack" "cloudfront" {
     StandardLoggingEnabled         = true
   }
   tags = local.default_tags
-  
+
   #ignoring below parameter as these parameter are been read via secret manager and terraform continually detects changes
   # Note : we need to remove the below lifecycle if the Header are changed in Secret manager to appy new cloainking header value 
   lifecycle {
-    ignore_changes = [parameters["OriginCloakingHeader"],parameters["PreviousOriginCloakingHeader"]]
+    ignore_changes = [parameters["OriginCloakingHeader"], parameters["PreviousOriginCloakingHeader"]]
   }
 
 }
 
 resource "aws_cloudformation_stack" "cloudfront-monitoring" {
-  count        = var.cloudfront_auth_frontend_enabled ? 1 : 0
-  provider     = aws.cloudfront
-  name         = "${var.environment}-auth-fe-cloudfront-monitoring"
+  count    = var.cloudfront_auth_frontend_enabled ? 1 : 0
+  provider = aws.cloudfront
+  name     = "${var.environment}-auth-fe-cloudfront-monitoring"
   #using fixed version of cloudfront monitoring  disturbution template for now 
   template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/cloudfront-monitoring-alarm/template.yaml?versionId=td2KHIlG7KGXl0mkMrRDkgBWxdXPEMZ."
 
