5a69a0ed3103d5daac6fa36b7d2740b38ec0761c
GTVJ <gwyn.jones@digital.cabinet-office.gov.uk>
2024-05-10 14:06:00 +0100

    AUT-2772: Add currentUrlMiddleware
    The new language toggle middleware requires a currentUrl property
    to be added to res.locals. This commit introduces the property along
    with associated tests to verify that it is being set as expected.

---
+++ test/unit/middleware/current-url-middleware.test.ts
@@ -0,0 +1,69 @@
+import { expect } from "chai";
+import { describe } from "mocha";
+import { NextFunction, Request, Response } from "express";
+import { sinon } from "../../utils/test-utils";
+import { setCurrentUrlMiddleware } from "../../../src/middleware/current-url-middleware";
+import { mockRequest, mockResponse } from "mock-req-res";
+
+describe("currentUrl middleware", () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: NextFunction;
+
+  const originalUrl = "/sign-in-or-create?lng=cy";
+  const host = "signin.account.gov.uk";
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe("setCurrentUrlMiddleware", () => {
+    describe("where req.i18n has been set", () => {
+      beforeEach(() => {
+        req = mockRequest({
+          originalUrl,
+          get: sinon.stub().withArgs("host").returns(host),
+          i18n: { language: "en" },
+        });
+        res = mockResponse();
+        next = sinon.fake() as unknown as NextFunction;
+      });
+
+      it("should add currentUrl to request locals", () => {
+        setCurrentUrlMiddleware(req as Request, res as Response, next);
+
+        expect(res.locals).to.have.property("currentUrl");
+      });
+
+      it("currentUrl should contain the scheme, host and originalUrl", () => {
+        setCurrentUrlMiddleware(req as Request, res as Response, next);
+        const currentUrlAsString = String(res.locals.currentUrl);
+
+        expect(currentUrlAsString).to.eq(`https://${host}${originalUrl}`);
+      });
+
+      it("should call next function", () => {
+        setCurrentUrlMiddleware(req as Request, res as Response, next);
+
+        expect(next).to.have.been.called;
+      });
+    });
+
+    describe("where req.i18n has not been set", () => {
+      before(() => {
+        req = mockRequest({
+          originalUrl,
+          get: sinon.stub().withArgs("host").returns(host),
+        });
+        res = mockResponse();
+        next = sinon.fake() as unknown as NextFunction;
+      });
+
+      it("should not add currentUrl to request locals", () => {
+        setCurrentUrlMiddleware(req as Request, res as Response, next);
+
+        expect(res.locals).to.not.have.property("currentUrl");
+      });
+    });
+  });
+});
