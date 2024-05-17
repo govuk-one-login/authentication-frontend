201e88f5ea1bfc43e550d0dca6fe92fd9f20df1a
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-05-17 12:32:42 +0100

    BAU: Remove unused service
    This is unused and redundant post the orch/auth split, and can be removed. This commit: https://github.com/govuk-one-login/authentication-frontend/commit/caa97dfdc98166187f4c23b2604cf2c0979148cc removed usage

--- src/components/prove-identity/types.ts
+++ src/components/prove-identity/types.ts
@@ -3,13 +3,3 @@ import { ApiResponseResult, DefaultApiResponse } from "../../types";
 export interface IPVAuthorisationResponse extends DefaultApiResponse {
   redirectUri?: string;
 }
-
-export interface ProveIdentityServiceInterface {
-  ipvAuthorize: (
-    sessionId: string,
-    clientSessionId: string,
-    emailAddress: string,
-    sourceIp: string,
-    persistentSessionId: string
-  ) => Promise<ApiResponseResult<IPVAuthorisationResponse>>;
-}
