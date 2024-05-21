06c0fa5102ab3a1a664448382ebf020b94f3eed9
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-05-21 18:48:20 +0100

    BAU: Use common variables in resend-mfa-controller tests
    * Cleans up some noisy setup
    * Makes tests clearer by re-using variables rather than hard-coding things multiple times
    * Makes tests easier to read by putting some noisy setup into a before block

--- src/components/account-creation/resend-mfa-code/tests/resend-mfa-code-controller.test.ts
+++ src/components/account-creation/resend-mfa-code/tests/resend-mfa-code-controller.test.ts
@@ -12,14 +12,21 @@ import { PATH_NAMES } from "../../../../app.constants";
 import { mockResponse, RequestOutput, ResponseOutput } from "mock-req-res";
 import { SendNotificationServiceInterface } from "../../../common/send-notification/types";
 import { createMockRequest } from "../../../../../test/helpers/mock-request-helper";
+import { commonVariables } from "../../../../../test/helpers/common-test-variables";
 
 describe("resend mfa controller", () => {
   let req: RequestOutput;
   let res: ResponseOutput;
+  const { sessionId, clientSessionId, diPersistentSessionId, ip, email } =
+    commonVariables;
 
   beforeEach(() => {
     req = createMockRequest(PATH_NAMES.CHECK_YOUR_PHONE);
+    req.ip = ip;
     res = mockResponse();
+    res.locals.sessionId = sessionId;
+    res.locals.clientSessionId = clientSessionId;
+    res.locals.persistentSessionId = diPersistentSessionId;
   });
 
   afterEach(() => {
@@ -44,10 +51,6 @@ describe("resend mfa controller", () => {
         }),
       } as unknown as SendNotificationServiceInterface;
 
-      res.locals.sessionId = "123456-djjad";
-      req.session.user = {
-        email: "test@test.com",
-      };
       req.path = PATH_NAMES.RESEND_MFA_CODE_ACCOUNT_CREATION;
 
       await resendMfaCodePost(fakeService)(req as Request, res as Response);
@@ -62,24 +65,20 @@ describe("resend mfa controller", () => {
           success: true,
         }),
       } as unknown as SendNotificationServiceInterface;
-      res.locals.sessionId = "123456-djjad";
-      res.locals.clientSessionId = "654321-djjad";
-      res.locals.persistentSessionId = "123123-djjad";
 
       req.session.user = {
-        email: "test@test.com",
+        email,
         isAccountRecoveryJourney: true,
       };
       req.path = PATH_NAMES.RESEND_MFA_CODE_ACCOUNT_CREATION;
-      req.ip = "127.0.0.1";
       await resendMfaCodePost(fakeService)(req as Request, res as Response);
       expect(fakeService.sendNotification).to.have.been.calledWith(
-        "123456-djjad",
-        "654321-djjad",
-        "test@test.com",
+        sessionId,
+        clientSessionId,
+        email,
         "VERIFY_PHONE_NUMBER",
-        "127.0.0.1",
-        "123123-djjad",
+        ip,
+        diPersistentSessionId,
         "",
         req,
         "ACCOUNT_RECOVERY"
@@ -92,24 +91,20 @@ describe("resend mfa controller", () => {
           success: true,
         }),
       } as unknown as SendNotificationServiceInterface;
-      res.locals.sessionId = "123456-djjad";
-      res.locals.clientSessionId = "654321-djjad";
-      res.locals.persistentSessionId = "123123-djjad";
 
       req.session.user = {
-        email: "test@test.com",
+        email,
         isAccountCreationJourney: true,
       };
       req.path = PATH_NAMES.RESEND_MFA_CODE_ACCOUNT_CREATION;
-      req.ip = "127.0.0.1";
       await resendMfaCodePost(fakeService)(req as Request, res as Response);
       expect(fakeService.sendNotification).to.have.been.calledWith(
-        "123456-djjad",
-        "654321-djjad",
-        "test@test.com",
+        sessionId,
+        clientSessionId,
+        email,
         "VERIFY_PHONE_NUMBER",
-        "127.0.0.1",
-        "123123-djjad",
+        ip,
+        diPersistentSessionId,
         "",
         req,
         "REGISTRATION"
