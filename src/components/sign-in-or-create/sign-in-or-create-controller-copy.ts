d6445b18dd98ffb0e59580f80ba3ec3fd711477d
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2024-05-21 17:24:14 +0100

    BAU: test sonarcloud quality gate

---
+++ src/components/sign-in-or-create/sign-in-or-create-controller-copy.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { getNextPathAndUpdateJourney } from "../common/constants";
+import { USER_JOURNEY_EVENTS } from "../common/state-machine/state-machine";
+
+export async function signInOrCreateGet(
+  req: Request,
+  res: Response
+): Promise<void> {
+  req.session.user.isAccountCreationJourney = false;
+  req.session.user.isPasswordResetJourney = false;
+  req.session.user.isSignInJourney = false;
+  if (req.query.redirectPost) {
+    return await signInOrCreatePost(req, res);
+  }
+  res.render("sign-in-or-create/index.njk", {
+    serviceType: req.session.client.serviceType,
+  });
+}
+
+export async function signInOrCreatePost(
+  req: Request,
+  res: Response
+): Promise<void> {
+  res.redirect(
+    await getNextPathAndUpdateJourney(
+      req,
+      req.path,
+      req.body.optionSelected === "create"
+        ? USER_JOURNEY_EVENTS.CREATE_NEW_ACCOUNT
+        : USER_JOURNEY_EVENTS.SIGN_IN,
+      null,
+      res.locals.sessionId
+    )
+  );
+}
