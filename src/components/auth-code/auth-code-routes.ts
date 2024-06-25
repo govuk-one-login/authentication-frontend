4cd21ec62ceae043cc89b6dacb5dcc31651423d1
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-06-25 09:54:30 +0100

    AUT-2908: Send authenticated field through on auth code account interventions request
    This parameter will control whether the TICF CRI is called - currently we only want to call it at the end of a journey when a user is authenticated, so updating the auth code middleware to pass this through.

--- src/components/auth-code/auth-code-routes.ts
+++ src/components/auth-code/auth-code-routes.ts
@@ -13,7 +13,7 @@ router.get(
   PATH_NAMES.AUTH_CODE,
   validateSessionMiddleware,
   allowUserJourneyMiddleware,
-  asyncHandler(accountInterventionsMiddleware(false, true)),
+  asyncHandler(accountInterventionsMiddleware(false, true, true)),
   asyncHandler(authCodeGet())
 );
 
