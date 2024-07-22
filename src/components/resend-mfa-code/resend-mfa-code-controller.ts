399095bbce10ab1421621e31ce76b79659b60b70
di-fabs <fabien.saujot@digital.cabinet-office.gov.uk>
2024-07-22 08:33:11 +0100

    Aut-3089: update resend mfa code paragraph content if reauth is activated
    Aut-3089: add isReauth journey

--- src/components/resend-mfa-code/resend-mfa-code-controller.ts
+++ src/components/resend-mfa-code/resend-mfa-code-controller.ts
@@ -3,10 +3,11 @@ import { ExpressRouteFunc } from "../../types";
 import { mfaService } from "../common/mfa/mfa-service";
 import { MfaServiceInterface } from "../common/mfa/types";
 import { sendMfaGeneric } from "../common/mfa/send-mfa-controller";
-import { PATH_NAMES } from "../../app.constants";
+import { JOURNEY_TYPE, PATH_NAMES } from "../../app.constants";
 import { pathWithQueryParam } from "../common/constants";
-import { support2hrLockout } from "../../config";
+import { support2hrLockout, supportReauthentication } from "../../config";
 import { isLocked } from "../../utils/lock-helper";
+import { getJourneyTypeFromUserSession } from "../common/journey/journey";
 
 export function resendMfaCodeGet(req: Request, res: Response): void {
   if (isLocked(req.session.user.wrongCodeEnteredLock)) {
@@ -30,10 +31,14 @@ export function resendMfaCodeGet(req: Request, res: Response): void {
       support2hrLockout: support2hrLockout(),
     });
   } else {
+    const journeyType = getJourneyTypeFromUserSession(req.session.user);
+
     res.render("resend-mfa-code/index.njk", {
       phoneNumber: req.session.user.redactedPhoneNumber,
       isResendCodeRequest: req.query?.isResendCodeRequest,
       support2hrLockout: support2hrLockout(),
+      supportReauthentication: supportReauthentication(),
+      isReauthJourney: journeyType === JOURNEY_TYPE.REAUTHENTICATION,
     });
   }
 }
