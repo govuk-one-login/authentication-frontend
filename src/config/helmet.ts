a07abe46006f02273576bf7f9f46371809bdc4a1
GTVJ <gwyn.jones@digital.cabinet-office.gov.uk>
2024-08-23 10:00:18 +0100

    BAU: Update Helmet, refactor config and remove deprecated option
    This commit:
    
    - Updates the version of helmet to match a recent dependabot
      PR (See #1716)
    - Removes the Certificate Transparency option from the config. It
      is now deprecated and not supported by helmet. See below.
    - Refactors the helmet configuration to use an object rather than
      a function. I've done this because the function felt verbose
    
    Expect-CT deprecated: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Expect-CT
    Helmet 7 removes support: https://github.com/helmetjs/helmet/issues/310

--- src/config/helmet.ts
+++ src/config/helmet.ts
@@ -1,76 +1,50 @@
-import helmet from "helmet";
-import e, { Request, Response } from "express";
-// Helmet does not export the config type - This is the way the recommend getting it on GitHub.
-export function helmetConfiguration(): Parameters<typeof helmet>[0] {
-  const helmetConfig: {
-    permittedCrossDomainPolicies: boolean;
-    referrerPolicy: boolean;
-    expectCt: boolean;
-    frameguard: { action: string };
-    hsts: { maxAge: number; includeSubDomains: boolean; preload: boolean };
-    dnsPrefetchControl: { allow: boolean };
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: string[];
-        objectSrc: string[];
-        styleSrc: string[];
-        scriptSrc: (string | ((req: e.Request, res: e.Response) => string))[];
-        imgSrc: string[];
-        connectSrc: string[];
-        "form-action"?: string[];
-        "frame-ancestors"?: string[];
-      };
-    };
-  } = {
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["'self'"],
-        scriptSrc: [
-          "'self'",
-          // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-          (req: Request, res: Response): string =>
-            `'nonce-${res.locals.scriptNonce}'`,
-          "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
-          "https://*.googletagmanager.com",
-          "https://www.google-analytics.com",
-          "https://ssl.google-analytics.com",
-        ],
-        imgSrc: [
-          "'self'",
-          "data:",
-          "https://*.google-analytics.com",
-          "https://*.googletagmanager.com",
-          "https://www.google-analytics.com",
-        ],
-        objectSrc: ["'none'"],
-        connectSrc: [
-          "'self'",
-          "https://*.google-analytics.com",
-          "https://*.analytics.google.com",
-          "https://*.googletagmanager.com",
-          "https://www.google-analytics.com",
-        ],
-      },
+import { HelmetOptions } from "helmet";
+import { Request, Response } from "express";
+
+export const helmetConfiguration: HelmetOptions = {
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'"],
+      scriptSrc: [
+        "'self'",
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+        (req: Request, res: Response): string =>
+          `'nonce-${res.locals.scriptNonce}'`,
+        "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
+        "https://*.googletagmanager.com",
+        "https://www.google-analytics.com",
+        "https://ssl.google-analytics.com",
+      ],
+      imgSrc: [
+        "'self'",
+        "data:",
+        "https://*.google-analytics.com",
+        "https://*.googletagmanager.com",
+        "https://www.google-analytics.com",
+      ],
+      objectSrc: ["'none'"],
+      connectSrc: [
+        "'self'",
+        "https://*.google-analytics.com",
+        "https://*.analytics.google.com",
+        "https://*.googletagmanager.com",
+        "https://www.google-analytics.com",
+      ],
+      frameAncestors: ["'self'", "https://*.account.gov.uk"],
     },
-    dnsPrefetchControl: {
-      allow: false,
-    },
-    frameguard: {
-      action: "deny",
-    },
-    hsts: {
-      maxAge: 31536000, // 1 Year
-      preload: true,
-      includeSubDomains: true,
-    },
-    referrerPolicy: false,
-    permittedCrossDomainPolicies: false,
-    expectCt: false,
-  };
-  helmetConfig.contentSecurityPolicy.directives["frame-ancestors"] = [
-    "'self'",
-    "https://*.account.gov.uk",
-  ];
-  return helmetConfig;
-}
+  },
+  dnsPrefetchControl: {
+    allow: false,
+  },
+  frameguard: {
+    action: "deny",
+  },
+  hsts: {
+    maxAge: 31536000, // 1 Year
+    preload: true,
+    includeSubDomains: true,
+  },
+  referrerPolicy: false,
+  permittedCrossDomainPolicies: false,
+};
