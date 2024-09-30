24d4ea11827c206c8a073be927252c832af33e6e
SophiaDWokoma <148252375+SophiaDWokoma@users.noreply.github.com>
2024-09-30 12:38:25 +0100

    [DFC-660]: add integration tests and update integration test command to its original state

--- src/components/common/layout/base-integration.test.ts
+++ src/components/common/layout/base-integration.test.ts
@@ -1,28 +1,38 @@
 import request from "supertest";
 import { describe } from "mocha";
+import { expect } from "chai";
+import * as cheerio from "cheerio";
 import { sinon } from "../../../../test/utils/test-utils";
 import nock = require("nock");
 import decache from "decache";
-import { PATH_NAMES } from "../../../app.constants";
+import { PATH_NAMES, CHANNEL } from "../../../app.constants";
 
 describe("Integration:: base page ", () => {
   let app: any;
 
-  before(async () => {
+  const setupApp = async (channel: string) => {
     decache("../../../app");
     decache("../../../middleware/session-middleware");
     const sessionMiddleware = require("../../../middleware/session-middleware");
-    // const channelMiddleware = require("../../../../middleware/channel-middleware");
     sinon
       .stub(sessionMiddleware, "validateSessionMiddleware")
       .callsFake(function (req: any, res: any, next: any): void {
         res.locals.sessionId = "tDy103saszhcxbQq0-mjdzU854";
+        if (channel === CHANNEL.WEB) {
+          res.locals.webChannel = true;
+        } else if (channel === CHANNEL.STRATEGIC_APP) {
+          res.locals.strategicAppChannel = true;
+        }
 
+        req.session.client = {
+          serviceType: "MANDATORY",
+        };
         req.session.user = {
           email: "test@test.com",
+
           phoneNumber: "7867",
           journey: {
-            nextPath: PATH_NAMES.ENTER_MFA,
+            nextPath: PATH_NAMES.SIGN_IN_OR_CREATE,
             optionalPaths: [PATH_NAMES.RESEND_MFA_CODE_ACCOUNT_CREATION],
           },
         };
@@ -31,13 +41,7 @@ describe("Integration:: base page ", () => {
       });
 
     app = await require("../../../app").createApp();
-
-    // await request(app)
-    //   .get(PATH_NAMES.SIGN_IN_OR_CREATE)
-    //   .then((res) => {
-    //     const $ = cheerio.load(res.text);
-    //   });
-  });
+  };
 
   beforeEach(() => {
     nock.cleanAll();
@@ -49,9 +53,21 @@ describe("Integration:: base page ", () => {
   });
 
   it("should return the sign in or create page", async () => {
-    await request(app).get(PATH_NAMES.SIGN_IN_OR_CREATE).expect(302);
+    await setupApp(CHANNEL.WEB);
+    await request(app).get(PATH_NAMES.SIGN_IN_OR_CREATE).expect(200);
   });
-  it("should return the sign in or create page", async () => {
-    await request(app).get(PATH_NAMES.SIGN_IN_OR_CREATE).then();
+  it("footer should be on page", async () => {
+    await setupApp(CHANNEL.WEB);
+    const response = await request(app).get(PATH_NAMES.SIGN_IN_OR_CREATE);
+    expect(response.status).to.equal(200);
+    const $ = cheerio.load(response.text);
+    expect($(".govuk-footer").length).to.equal(1);
+  });
+  it("footer should not be on page", async () => {
+    await setupApp(CHANNEL.STRATEGIC_APP);
+    const response = await request(app).get(PATH_NAMES.SIGN_IN_OR_CREATE);
+    expect(response.status).to.equal(200);
+    const $ = cheerio.load(response.text);
+    expect($(".govuk-footer").length).to.equal(0);
   });
 });
