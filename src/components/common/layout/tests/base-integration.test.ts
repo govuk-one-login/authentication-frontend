c663cd5b679a89960384b1a698f88f8451f3dcdf
SophiaDWokoma <148252375+SophiaDWokoma@users.noreply.github.com>
2024-09-30 16:36:16 +0100

    [DFC-661]: Add conditional rendering for beta banner and integration testing

---
+++ src/components/common/layout/tests/base-integration.test.ts
@@ -0,0 +1,69 @@
+import request from "supertest";
+import { describe } from "mocha";
+import { expect } from "chai";
+import * as cheerio from "cheerio";
+import { sinon } from "../../../../../test/utils/test-utils";
+import nock = require("nock");
+import decache from "decache";
+import { PATH_NAMES, CHANNEL } from "../../../../app.constants";
+
+describe("Integration:: base page ", () => {
+  let app: any;
+
+  const setupApp = async (channel: string) => {
+    decache("../../../../app");
+    decache("../../../../middleware/session-middleware");
+    const sessionMiddleware = require("../../../../middleware/session-middleware");
+    sinon
+      .stub(sessionMiddleware, "validateSessionMiddleware")
+      .callsFake(function (req: any, res: any, next: any): void {
+        res.locals.sessionId = "tDy103saszhcxbQq0-mjdzU854";
+        if (channel === CHANNEL.WEB) {
+          res.locals.webChannel = true;
+        } else if (channel === CHANNEL.STRATEGIC_APP) {
+          res.locals.strategicAppChannel = true;
+        }
+
+        req.session.client = {
+          serviceType: "MANDATORY",
+        };
+        req.session.user = {
+          email: "test@test.com",
+
+          phoneNumber: "7867",
+          journey: {
+            nextPath: PATH_NAMES.SIGN_IN_OR_CREATE,
+          },
+        };
+
+        next();
+      });
+
+    app = await require("../../../../app").createApp();
+  };
+
+  beforeEach(() => {
+    nock.cleanAll();
+  });
+
+  after(() => {
+    sinon.restore();
+    app = undefined;
+  });
+
+  it("The beta banner should appear on the page when the channel is set to 'web'", async () => {
+    await setupApp(CHANNEL.WEB);
+    const response = await request(app).get(PATH_NAMES.SIGN_IN_OR_CREATE);
+    expect(response.status).to.equal(200);
+    const $ = cheerio.load(response.text);
+    expect($(".govuk-phase-banner").length).to.equal(1);
+  });
+
+  it("The beta banner should not appear on the page when the channel is set to 'strategic_app'", async () => {
+    await setupApp(CHANNEL.STRATEGIC_APP);
+    const response = await request(app).get(PATH_NAMES.SIGN_IN_OR_CREATE);
+    expect(response.status).to.equal(200);
+    const $ = cheerio.load(response.text);
+    expect($(".govuk-phase-banner").length).to.equal(0);
+  });
+});
