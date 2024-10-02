cce9aee85c81ba8f3dda41bd9facf06c3ca42ff6
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-10-02 11:46:32 +0100

    BAU: Adds start request parameter type
    The interface for the authorize service is looking quite bloated
    as it now takes 8 parameters. This type will be implemented here to
    move some of the parameters into an object, so we can make this
    interface a little less crowded

--- src/components/authorize/types.ts
+++ src/components/authorize/types.ts
@@ -2,6 +2,14 @@ import { ApiResponseResult, DefaultApiResponse } from "../../types";
 import { Claims } from "./claims-config";
 import { Request } from "express";
 
+export interface StartRequestParameters {
+  authenticated: boolean;
+  previous_session_id?: string;
+  rp_pairwise_id_for_reauth?: string;
+  previous_govuk_signin_journey_id?: string;
+  reauthenticate?: string;
+}
+
 export interface StartAuthResponse extends DefaultApiResponse {
   user: UserSessionInfo;
   featureFlags?: Record<string, unknown>;
