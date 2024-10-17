d18a4e677a28be81d183bc9933c1afeaa6243cc3
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2024-10-17 08:50:09 +0100

    AUT-3762: Extract server start logic to tested method
    Logic will be added soon that will gracefully shutdown the server on SIGTERM. To ensure there is no regression, this should be unit tested.
    
    Before doing that, make the existing start server logic testable and add tests to assert the server starts on the expected port, with the expected timeouts and with the vital-signs package.

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,9 @@
-import { createApp } from "./app";
+import { createApp, startServer } from "./app";
 import { logger } from "./utils/logger";
-import { frontendVitalSignsInit } from "@govuk-one-login/frontend-vital-signs";
-
-const port: number | string = process.env.PORT || 3000;
 
 (async () => {
   const app = await createApp();
-  const server = app
-    .listen(port, () => {
-      logger.info(`Server listening on port ${port}`);
-      app.emit("appStarted");
-    })
-    .on("error", (error: Error) => {
-      logger.error(`Unable to start server because of ${error.message}`);
-    });
-  server.keepAliveTimeout = 61 * 1000;
-  server.headersTimeout = 91 * 1000;
-  frontendVitalSignsInit(server, {
-    staticPaths: [/^\/assets\/.*/, /^\/public\/.*/],
-  });
+  await startServer(app);
 })().catch((ex) => {
   logger.error(`Server failed to create app ${ex.message}`);
 });
