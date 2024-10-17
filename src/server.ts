9f9f36ea2ce79af3d34f56eec738764d161ed62b
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2024-10-17 12:07:33 +0100

    AUT-3762: Graceful shutdown on SIGTERM and SIGINT
    Should the process receive a signal indicating it should shutdown, the `closeServer()` method is run that will systematically close down the server. The process is then exited with a success code (0).
    
    If the shutdown fails for some reason, we exit the process with an error code (1).

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import { createApp, startServer } from "./app";
+import { createApp, shutdownProcess, startServer } from "./app";
 import { logger } from "./utils/logger";
 
 (async () => {
   const app = await createApp();
-  await startServer(app);
+  const { closeServer } = await startServer(app);
+  const shutdown = shutdownProcess(closeServer);
+  process.on("SIGTERM", shutdown);
+  process.on("SIGINT", shutdown);
 })().catch((ex) => {
   logger.error(`Server failed to create app ${ex.message}`);
 });
