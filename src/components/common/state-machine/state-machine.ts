4f6fcc4b1da6c3d93e6e73b159ef07f6f157c471
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2024-11-21 18:50:29 +0000

    AUT-3893: Remove prove identity welcome from the state machine

--- src/components/common/state-machine/state-machine.ts
+++ src/components/common/state-machine/state-machine.ts
@@ -4,7 +4,6 @@ import {
   OIDC_PROMPT,
   PATH_NAMES,
 } from "../../../app.constants";
-import { proveIdentityWelcomeEnabled } from "../../../config";
 
 const USER_JOURNEY_EVENTS = {
   AUTHENTICATED: "AUTHENTICATED",
@@ -80,7 +79,6 @@ const authStateMachine = createMachine(
       requiresResetPasswordMFASmsCode: false,
       requiresResetPasswordMFAAuthAppCode: false,
       isOnForcedPasswordResetJourney: false,
-      proveIdentityWelcomeEnabled: proveIdentityWelcomeEnabled(),
     },
     states: {
       [PATH_NAMES.ROOT]: {
@@ -93,10 +91,6 @@ const authStateMachine = createMachine(
       [PATH_NAMES.AUTHORIZE]: {
         on: {
           [USER_JOURNEY_EVENTS.EXISTING_SESSION]: [
-            {
-              target: [PATH_NAMES.PROVE_IDENTITY_WELCOME],
-              cond: "isIdentityRequiredAndProveIdentityWelcomeEnabled",
-            },
             { target: [PATH_NAMES.ENTER_PASSWORD], cond: "requiresLogin" },
             {
               target: [PATH_NAMES.ENTER_AUTHENTICATOR_APP_CODE],
@@ -122,10 +116,6 @@ const authStateMachine = createMachine(
               target: [PATH_NAMES.DOC_CHECKING_APP],
               cond: "skipAuthentication",
             },
-            {
-              target: [PATH_NAMES.PROVE_IDENTITY_WELCOME],
-              cond: "isIdentityRequiredAndProveIdentityWelcomeEnabled",
-            },
             { target: [PATH_NAMES.SIGN_IN_OR_CREATE] },
           ],
         },
@@ -135,25 +125,6 @@ const authStateMachine = createMachine(
           [USER_JOURNEY_EVENTS.SIGN_IN]: [PATH_NAMES.ENTER_EMAIL_SIGN_IN],
           [USER_JOURNEY_EVENTS.CREATE_NEW_ACCOUNT]: [
             PATH_NAMES.ENTER_EMAIL_CREATE_ACCOUNT,
-          ],
-        },
-      },
-      [PATH_NAMES.PROVE_IDENTITY_WELCOME]: {
-        on: {
-          [USER_JOURNEY_EVENTS.EXISTING_SESSION]: [
-            {
-              target: [PATH_NAMES.ENTER_PASSWORD],
-              cond: "requiresLogin",
-            },
-            {
-              target: [PATH_NAMES.ENTER_AUTHENTICATOR_APP_CODE],
-              cond: "requiresAuthAppUplift",
-            },
-            { target: [PATH_NAMES.UPLIFT_JOURNEY], cond: "requiresUplift" },
-            { target: [PATH_NAMES.PROVE_IDENTITY] },
-          ],
-          [USER_JOURNEY_EVENTS.CREATE_OR_SIGN_IN]: [
-            PATH_NAMES.SIGN_IN_OR_CREATE,
           ],
         },
       },
@@ -764,9 +735,6 @@ const authStateMachine = createMachine(
       skipAuthentication: (context) =>
         context.skipAuthentication === true &&
         context.isAuthenticated === false,
-      isIdentityRequiredAndProveIdentityWelcomeEnabled: (context) =>
-        context.isIdentityRequired === true &&
-        context.proveIdentityWelcomeEnabled === true,
       requiresMFAAuthAppCode: (context) =>
         context.mfaMethodType === MFA_METHOD_TYPE.AUTH_APP &&
         context.requiresTwoFactorAuth === true,
