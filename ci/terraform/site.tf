f569c4cf09c16edf833eab4b4b7415aaf25bde3c
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2024-12-10 17:06:29 +0000

    BAU: Update terraform versions

--- ci/terraform/site.tf
+++ ci/terraform/site.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.7.1"
+  required_version = ">= 1.9.8"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 5.45.0"
+      version = "5.75.1"
     }
     random = {
       source  = "hashicorp/random"
