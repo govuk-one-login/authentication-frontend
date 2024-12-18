aedd9e8c398bd8a42adbb414263d5a6060ca49de
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2024-12-18 13:18:36 +0000

    BAU: Template validation logs properties accessed
    First step to validating properties have values is to collect a list of properties accessed in a template.
    
    We do this here by asking `nunjucks` to compile the provided template (which creates JavaScript as a string), then ask `acorn` to create an AST from the compiled JavaScript string. `acorn` is the same library used by `prettier` to process JavaScript code.
    
    `acorn-walk` is then used to evaluate the template and pull out references both translation keys and variables accessed in the template.
    
    These are debugged logged for now.
    
    Later commits will evaluate if the accessed properties have values, and warn if not.

---
+++ src/middleware/tests/template-validation-middleware.test.ts
@@ -0,0 +1,52 @@
+import chai, { expect } from "chai";
+import { Request, Response } from "express";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+import { describe } from "mocha";
+import { mockRequest, mockResponse } from "mock-req-res";
+import { templateValidationMiddleware } from "../template-validation-middleware";
+
+chai.use(sinonChai);
+
+describe("templateValidationMiddleware", () => {
+  const BASIC_TEMPLATE_PATH =
+    "../middleware/tests/template-validation-basic-test.njk";
+  const debugLog = sinon.spy();
+  const next = sinon.spy();
+  let req: Request;
+  let res: Response;
+
+  beforeEach(() => {
+    req = mockRequest({
+      session: { client: {}, user: {} },
+      log: {
+        debug: debugLog,
+      },
+    });
+    res = mockResponse();
+    templateValidationMiddleware(req as Request, res as Response, next);
+  });
+
+  it("logs looked up properties in a template", () => {
+    res.render(BASIC_TEMPLATE_PATH);
+
+    expect(debugLog).to.have.been.calledWithExactly(
+      `template "${BASIC_TEMPLATE_PATH}" looks up these properties ${JSON.stringify(
+        {
+          variableNames: [
+            "ga4OnPageLoad",
+            "scriptNonce",
+            "pageTitleName",
+            "taxonomyLevel1",
+            "taxonomyLevel2",
+            "contentId",
+          ],
+          translationKeys: [
+            "pages.mandatory.title",
+            "pages.someExampleTextToTranslate",
+          ],
+        }
+      )}`
+    );
+  });
+});
