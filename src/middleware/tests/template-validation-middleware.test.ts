71889f958272f798ff4ef1a76799892d10e7d5d5
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2024-12-18 13:54:05 +0000

    BAU: Validator warns of accesses to properties with no value
    Template validator now warns when the template looks up a property that is not available in the request context, either a variable or template key.
    
    This can be used to find rendered templates with potentially incorrect values set, with the idea to fix any issues found then make the validator throw an error instead of warn.
    
    This wll allow devs and test runs to see quickly when a variable is accessed, but has no value.

--- src/middleware/tests/template-validation-middleware.test.ts
+++ src/middleware/tests/template-validation-middleware.test.ts
@@ -11,19 +11,40 @@ chai.use(sinonChai);
 describe("templateValidationMiddleware", () => {
   const BASIC_TEMPLATE_PATH =
     "../middleware/tests/template-validation-basic-test.njk";
-  const debugLog = sinon.spy();
-  const next = sinon.spy();
+  let debugLog: sinon.SinonSpy;
+  let warnLog: sinon.SinonSpy;
+  let next: sinon.SinonSpy;
   let req: Request;
   let res: Response;
+  let translations: { [key: string]: string };
+  let options: { [key: string]: string };
 
   beforeEach(() => {
+    translations = {
+      "pages.mandatory.title": "A title",
+      "pages.someExampleTextToTranslate": "Something to translate",
+    };
+    options = {
+      scriptNonce: "123456",
+      taxonomyLevel1: "template",
+      taxonomyLevel2: "validation",
+      contentId: "fake-content",
+    };
+    debugLog = sinon.spy();
+    warnLog = sinon.spy();
+    next = sinon.spy();
     req = mockRequest({
       session: { client: {}, user: {} },
       log: {
         debug: debugLog,
+        warn: warnLog,
       },
     });
-    res = mockResponse();
+    res = mockResponse({
+      locals: {
+        t: (key: string) => translations[key] || key,
+      },
+    });
     templateValidationMiddleware(req as Request, res as Response, next);
   });
 
@@ -45,6 +66,26 @@ describe("templateValidationMiddleware", () => {
           ],
         }
       )}`
+    );
+  });
+
+  it("does not log if all properties access have a value", () => {
+    res.render(BASIC_TEMPLATE_PATH, options);
+
+    expect(warnLog).to.have.been.callCount(0);
+  });
+
+  it("warns if template accesses properties that do not exist", () => {
+    delete translations["pages.someExampleTextToTranslate"];
+    delete options.taxonomyLevel2;
+
+    res.render(BASIC_TEMPLATE_PATH, options);
+
+    expect(warnLog).to.have.been.calledWithExactly(
+      `template "${BASIC_TEMPLATE_PATH}" uses translation key "pages.someExampleTextToTranslate" that has no value`
+    );
+    expect(warnLog).to.have.been.calledWithExactly(
+      `template "${BASIC_TEMPLATE_PATH}" uses variable name "taxonomyLevel2" that has no value`
     );
   });
 });
