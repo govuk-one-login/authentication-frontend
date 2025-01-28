319f033ecedd7c3fa6f848c49b45ec89a8c2aa78
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2025-01-28 14:35:20 +0000

    BAU: Extract out construction of fake password service
    Cleans up a bit of noise in tests. I added nesting here to add the helper function at the relevant level
    
    Also moves some common request setup to a before block

--- src/components/reset-password/tests/reset-password-controller.test.ts
+++ src/components/reset-password/tests/reset-password-controller.test.ts
@@ -85,159 +85,151 @@ describe("reset password controller (in 6 digit code flow)", () => {
     });
   });
 
-  TEST_SCENARIO_PARAMETERS.forEach(function (params) {
-    describe(
-      "resetPasswordPost supportPasswordResetRequired: " +
-        params.supportPasswordResetRequired +
-        " passwordChangeRequired: " +
-        params.passwordChangeRequired,
-      () => {
-        it("should redirect to /auth-code if request is success", async () => {
-          const fakeResetService: ResetPasswordServiceInterface = {
-            updatePassword: sinon.fake.returns({ success: true }),
-          } as unknown as ResetPasswordServiceInterface;
-          const fakeLoginService: EnterPasswordServiceInterface = {
-            loginUser: sinon.fake.returns({
-              success: true,
-              data: {
-                redactedPhoneNumber: "1234",
-                latestTermsAndConditionsAccepted: true,
-                mfaMethodVerified: true,
-                mfaMethodType: MFA_METHOD_TYPE.SMS,
-                mfaRequired: true,
-                passwordChangeRequired: params.passwordChangeRequired,
-              },
-            }),
-          } as unknown as EnterPasswordServiceInterface;
-
-          await resetPasswordPost(fakeResetService, fakeLoginService)(
-            req as Request,
-            res as Response
-          );
-
-          expect(res.redirect).to.have.calledWith(PATH_NAMES.AUTH_CODE);
-        });
-
-        it("should redirect to /get-security-codes when password updated and mfa method not verified", async () => {
-          const fakeResetService: ResetPasswordServiceInterface = {
-            updatePassword: sinon.fake.returns({ success: true }),
-          } as unknown as ResetPasswordServiceInterface;
-          const fakeLoginService: EnterPasswordServiceInterface = {
-            loginUser: sinon.fake.returns({
-              success: true,
-              data: {
-                redactedPhoneNumber: "1234",
-                latestTermsAndConditionsAccepted: true,
-                mfaMethodVerified: false,
-                mfaRequired: true,
-                passwordChangeRequired: params.passwordChangeRequired,
-              },
-            }),
-          } as unknown as EnterPasswordServiceInterface;
-
-          req.session.user = {
-            email: "joe.bloggs@test.com",
-          };
-          req.body.password = "Password1";
-
-          await resetPasswordPost(fakeResetService, fakeLoginService)(
-            req as Request,
-            res as Response
-          );
-
-          expect(fakeResetService.updatePassword).to.have.been.calledOnce;
-          expect(fakeLoginService.loginUser).to.have.been.calledOnce;
-
-          expect(res.redirect).to.have.calledWith(
-            PATH_NAMES.GET_SECURITY_CODES
-          );
-        });
-      }
-    );
-
-    it("should not set the passwordResetTime flag on the session if update password is not a success", async () => {
-      const fakeResetService: ResetPasswordServiceInterface = {
-        updatePassword: sinon.fake.returns({
-          success: false,
-          data: { code: ERROR_CODES.NEW_PASSWORD_SAME_AS_EXISTING },
-        }),
+  describe("resetPasswordPost", () => {
+    const newPassword = "Password1";
+    function fakeResetServiceReturningSuccess(success: boolean, data?: object) {
+      return {
+        updatePassword: sinon.fake.returns({ success, data }),
       } as unknown as ResetPasswordServiceInterface;
-      const fakeLoginService: EnterPasswordServiceInterface = {
-        loginUser: sinon.fake.returns({
-          success: true,
-          data: {
-            redactedPhoneNumber: "1234",
-            latestTermsAndConditionsAccepted: true,
-            mfaMethodVerified: true,
-            mfaRequired: false,
-            mfaMethodType: MFA_METHOD_TYPE.SMS,
-            passwordChangeRequired: params.passwordChangeRequired,
-          },
-        }),
-      } as unknown as EnterPasswordServiceInterface;
+    }
 
+    beforeEach(() => {
       req.session.user = {
         email: "joe.bloggs@test.com",
       };
-      req.body.password = "Password1";
-
-      await resetPasswordPost(fakeResetService, fakeLoginService)(
-        req as Request,
-        res as Response
-      );
-
-      expect(fakeResetService.updatePassword).to.have.been.calledOnce;
-      expect(fakeLoginService.loginUser).not.to.have.been.called;
-
-      expect(req.session.user.passwordResetTime).to.be.undefined;
+      req.body.password = newPassword;
     });
-  });
 
-  [true, false].forEach(function (supportMfaResetWithIpv) {
-    it(`should call the update password service with the value of allowMfaResetAfterPasswordReset set ${supportMfaResetWithIpv} when this is the value of the mfa reset feature flag`, async () => {
-      if (supportMfaResetWithIpv) {
-        process.env.SUPPORT_MFA_RESET_WITH_IPV = "1";
-      } else {
-        process.env.SUPPORT_MFA_RESET_WITH_IPV = "0";
-      }
-      const fakeResetService: ResetPasswordServiceInterface = {
-        updatePassword: sinon.fake.returns({ success: true }),
-      } as unknown as ResetPasswordServiceInterface;
-      const fakeLoginService: EnterPasswordServiceInterface = {
-        loginUser: sinon.fake.returns({
-          success: true,
-          data: {
-            redactedPhoneNumber: "1234",
-            latestTermsAndConditionsAccepted: true,
-            mfaMethodVerified: false,
-            mfaRequired: true,
-            passwordChangeRequired: false,
-          },
-        }),
-      } as unknown as EnterPasswordServiceInterface;
+    TEST_SCENARIO_PARAMETERS.forEach(function (params) {
+      describe(
+        "resetPasswordPost supportPasswordResetRequired: " +
+          params.supportPasswordResetRequired +
+          " passwordChangeRequired: " +
+          params.passwordChangeRequired,
+        () => {
+          it("should redirect to /auth-code if request is success", async () => {
+            const fakeResetService = fakeResetServiceReturningSuccess(true);
+            const fakeLoginService: EnterPasswordServiceInterface = {
+              loginUser: sinon.fake.returns({
+                success: true,
+                data: {
+                  redactedPhoneNumber: "1234",
+                  latestTermsAndConditionsAccepted: true,
+                  mfaMethodVerified: true,
+                  mfaMethodType: MFA_METHOD_TYPE.SMS,
+                  mfaRequired: true,
+                  passwordChangeRequired: params.passwordChangeRequired,
+                },
+              }),
+            } as unknown as EnterPasswordServiceInterface;
 
-      req.session.user = {
-        email: "joe.bloggs@test.com",
-      };
-      req.body.password = "Password1";
+            await resetPasswordPost(fakeResetService, fakeLoginService)(
+              req as Request,
+              res as Response
+            );
 
-      await resetPasswordPost(fakeResetService, fakeLoginService)(
-        req as Request,
-        res as Response
+            expect(res.redirect).to.have.calledWith(PATH_NAMES.AUTH_CODE);
+          });
+
+          it("should redirect to /get-security-codes when password updated and mfa method not verified", async () => {
+            const fakeResetService = fakeResetServiceReturningSuccess(true);
+            const fakeLoginService: EnterPasswordServiceInterface = {
+              loginUser: sinon.fake.returns({
+                success: true,
+                data: {
+                  redactedPhoneNumber: "1234",
+                  latestTermsAndConditionsAccepted: true,
+                  mfaMethodVerified: false,
+                  mfaRequired: true,
+                  passwordChangeRequired: params.passwordChangeRequired,
+                },
+              }),
+            } as unknown as EnterPasswordServiceInterface;
+
+            await resetPasswordPost(fakeResetService, fakeLoginService)(
+              req as Request,
+              res as Response
+            );
+
+            expect(fakeResetService.updatePassword).to.have.been.calledOnce;
+            expect(fakeLoginService.loginUser).to.have.been.calledOnce;
+
+            expect(res.redirect).to.have.calledWith(
+              PATH_NAMES.GET_SECURITY_CODES
+            );
+          });
+        }
       );
 
-      expect(fakeResetService.updatePassword).to.have.been.calledOnceWith(
-        "Password1",
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        supportMfaResetWithIpv,
-        req
-      );
-      expect(fakeLoginService.loginUser).to.have.been.calledOnce;
+      it("should not set the passwordResetTime flag on the session if update password is not a success", async () => {
+        const fakeResetService = fakeResetServiceReturningSuccess(false, {
+          code: ERROR_CODES.NEW_PASSWORD_SAME_AS_EXISTING,
+        });
+        const fakeLoginService: EnterPasswordServiceInterface = {
+          loginUser: sinon.fake.returns({
+            success: true,
+            data: {
+              redactedPhoneNumber: "1234",
+              latestTermsAndConditionsAccepted: true,
+              mfaMethodVerified: true,
+              mfaRequired: false,
+              mfaMethodType: MFA_METHOD_TYPE.SMS,
+              passwordChangeRequired: params.passwordChangeRequired,
+            },
+          }),
+        } as unknown as EnterPasswordServiceInterface;
 
-      expect(res.redirect).to.have.calledWith(PATH_NAMES.GET_SECURITY_CODES);
+        await resetPasswordPost(fakeResetService, fakeLoginService)(
+          req as Request,
+          res as Response
+        );
+
+        expect(fakeResetService.updatePassword).to.have.been.calledOnce;
+        expect(fakeLoginService.loginUser).not.to.have.been.called;
+
+        expect(req.session.user.passwordResetTime).to.be.undefined;
+      });
+    });
+
+    [true, false].forEach(function (supportMfaResetWithIpv) {
+      it(`should call the update password service with the value of allowMfaResetAfterPasswordReset set ${supportMfaResetWithIpv} when this is the value of the mfa reset feature flag`, async () => {
+        if (supportMfaResetWithIpv) {
+          process.env.SUPPORT_MFA_RESET_WITH_IPV = "1";
+        } else {
+          process.env.SUPPORT_MFA_RESET_WITH_IPV = "0";
+        }
+        const fakeResetService = fakeResetServiceReturningSuccess(true);
+        const fakeLoginService: EnterPasswordServiceInterface = {
+          loginUser: sinon.fake.returns({
+            success: true,
+            data: {
+              redactedPhoneNumber: "1234",
+              latestTermsAndConditionsAccepted: true,
+              mfaMethodVerified: false,
+              mfaRequired: true,
+              passwordChangeRequired: false,
+            },
+          }),
+        } as unknown as EnterPasswordServiceInterface;
+
+        await resetPasswordPost(fakeResetService, fakeLoginService)(
+          req as Request,
+          res as Response
+        );
+
+        expect(fakeResetService.updatePassword).to.have.been.calledOnceWith(
+          newPassword,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          supportMfaResetWithIpv,
+          req
+        );
+        expect(fakeLoginService.loginUser).to.have.been.calledOnce;
+
+        expect(res.redirect).to.have.calledWith(PATH_NAMES.GET_SECURITY_CODES);
+      });
     });
   });
 });
