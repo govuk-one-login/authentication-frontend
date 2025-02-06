921ecfe21f1bad1841d498d84da60df4aa04ed0a
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2025-02-06 11:45:22 +0000

    AUT-4051: Add new route for mfa reset guidance page
    This page will eventually be displayed to users attempting to reset their mfa via the new ipv journey when using the strategic app. Instead of allowing the users to reset their mfa, we will instead advise them to do the journey via a web browser.
    
    This adds a route and bare bones page. Routing into the journey, and populating this page with the correct information, will follow in subsequent commits

--- src/components/mfa-reset-with-ipv/mfa-reset-with-ipv-routes.ts
+++ src/components/mfa-reset-with-ipv/mfa-reset-with-ipv-routes.ts
@@ -3,7 +3,10 @@ import { PATH_NAMES } from "../../app.constants";
 import { validateSessionMiddleware } from "../../middleware/session-middleware";
 import { allowUserJourneyMiddleware } from "../../middleware/allow-user-journey-middleware";
 import { asyncHandler } from "../../utils/async";
-import { mfaResetWithIpvGet } from "./mfa-reset-with-ipv-controller";
+import {
+  mfaResetOpenInBrowserGet,
+  mfaResetWithIpvGet,
+} from "./mfa-reset-with-ipv-controller";
 
 const router = express.Router();
 
@@ -12,6 +15,12 @@ router.get(
   validateSessionMiddleware,
   allowUserJourneyMiddleware,
   asyncHandler(mfaResetWithIpvGet())
+);
+
+router.get(
+  PATH_NAMES.OPEN_IN_WEB_BROWSER,
+  validateSessionMiddleware, //TODO journey middleware
+  asyncHandler(mfaResetOpenInBrowserGet())
 );
 
 export { router as mfaResetWithIpvRouter };
