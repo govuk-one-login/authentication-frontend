674835c99a60a10303fba9e538f7295a605564c0
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2025-02-06 15:22:45 +0000

    BAU: Add integration test for existing mfa reset with ipv route

--- src/components/mfa-reset-with-ipv/tests/mfa-reset-with-ipv-integration.test.ts
+++ src/components/mfa-reset-with-ipv/tests/mfa-reset-with-ipv-integration.test.ts
@@ -3,14 +3,13 @@ import { expect, sinon, request } from "../../../../test/utils/test-utils";
 import nock = require("nock");
 import * as cheerio from "cheerio";
 import decache from "decache";
-import { PATH_NAMES } from "../../../app.constants";
+import { API_ENDPOINTS, PATH_NAMES } from "../../../app.constants";
+
+const IPV_DUMMY_URL =
+  "https://test-idp-url.com/callback?code=123-4ddkk0sdkkd-ad";
 
 describe("Mfa reset with ipv", () => {
-  let app: any;
-
-  before(async () => {
-    process.env.SUPPORT_MFA_RESET_WITH_IPV = "1";
-
+  async function setupAppWithSessionMiddleware(currentNextPath: string) {
     decache("../../../app");
     decache("../../../middleware/session-middleware");
     const sessionMiddleware = require("../../../middleware/session-middleware");
@@ -26,41 +25,91 @@ describe("Mfa reset with ipv", () => {
         req.session.user = {
           email: "test@test.com",
           journey: {
-            nextPath: PATH_NAMES.OPEN_IN_WEB_BROWSER,
+            nextPath: currentNextPath,
+            optionalPaths: [],
           },
           mfaMethodType: "AUTH_APP",
+          isAccountRecoveryPermitted: true,
+        };
+
+        req.session.client = {
+          redirectUri: "http://test-redirect.gov.uk/callback",
         };
 
         next();
       });
 
-    app = await require("../../../app").createApp();
+    return await require("../../../app").createApp();
+  }
+
+  describe("Mfa reset with ipv get", () => {
+    let app: any;
+    let baseApi: string;
+
+    before(async () => {
+      app = await setupAppWithSessionMiddleware(PATH_NAMES.MFA_RESET_WITH_IPV);
+      baseApi = process.env.FRONTEND_API_BASE_URL;
+    });
+
+    beforeEach(() => {
+      nock.cleanAll();
+    });
+
+    after(() => {
+      sinon.restore();
+      app = undefined;
+      delete process.env.SUPPORT_MFA_RESET_WITH_IPV;
+    });
+
+    it("should redirect to the mfa reset ipv path", async () => {
+      nock(baseApi).post(API_ENDPOINTS.MFA_RESET_AUTHORIZE).once().reply(200, {
+        authorize_url: IPV_DUMMY_URL,
+        code: 200,
+        success: true,
+      });
+      await request(app, (test) =>
+        test
+          .get(PATH_NAMES.MFA_RESET_WITH_IPV)
+          .expect(302)
+          .expect("Location", IPV_DUMMY_URL)
+      );
+    });
   });
 
-  beforeEach(() => {
-    nock.cleanAll();
-  });
+  describe("open in web browser", () => {
+    let app: any;
 
-  after(() => {
-    sinon.restore();
-    app = undefined;
-    delete process.env.SUPPORT_MFA_RESET_WITH_IPV;
-  });
+    before(async () => {
+      process.env.SUPPORT_MFA_RESET_WITH_IPV = "1";
 
-  it("should render the guidance page for when someone is trying to access via an app journey", async () => {
-    await request(app, (test) =>
-      test
-        .get(PATH_NAMES.OPEN_IN_WEB_BROWSER)
-        .expect(200)
-        .expect(function (res) {
-          const page = cheerio.load(res.text);
-          expect(page("h1").text()).to.contain(
-            "Open GOV.UK One Login in a web browser to continue"
-          );
-          expect(page("a.govuk-back-link").attr("href")).to.eq(
-            PATH_NAMES.ENTER_AUTHENTICATOR_APP_CODE
-          );
-        })
-    );
+      app = await setupAppWithSessionMiddleware(PATH_NAMES.OPEN_IN_WEB_BROWSER);
+    });
+
+    beforeEach(() => {
+      nock.cleanAll();
+    });
+
+    after(() => {
+      sinon.restore();
+      app = undefined;
+      delete process.env.SUPPORT_MFA_RESET_WITH_IPV;
+    });
+
+    it("should render the guidance page for when someone is trying to access via an app journey", async () => {
+      await request(app, (test) =>
+        test
+          .get(PATH_NAMES.OPEN_IN_WEB_BROWSER)
+          .expect(200)
+          .expect(function (res) {
+            const page = cheerio.load(res.text);
+            expect(page("h1").text()).to.contain(
+              "Open GOV.UK One Login in a web browser to continue"
+            );
+            expect(page("a.govuk-back-link").attr("href")).to.eq(
+              PATH_NAMES.ENTER_AUTHENTICATOR_APP_CODE
+            );
+          })
+      );
+    });
   });
 });
