03e2abb120123235273b60cf43eec461073841d1
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2025-03-06 19:12:03 +0000

    experiment with otel automatic instrumentation

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import { createApp, shutdownProcess, startServer } from "./app";
 import { logger } from "./utils/logger";
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node"; // configure the SDK to export telemetry data to the console
+// initialize the SDK and register with the OpenTelemetry API
+// this enables the API to record telemetry
+// enable all auto-instrumentations from the meta package
+new NodeSDK({
+  instrumentations: [getNodeAutoInstrumentations()],
+}).start();
 
 (async () => {
   const app = await createApp();
