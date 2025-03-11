7a04602bcd7c4046b340323972fcc26d0fe4c14e
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2025-03-11 14:46:09 +0000

    AUT-4149: wrap landing get
    This allows express to pick up async errors which occur in this controller

--- src/components/landing/landing-route.ts
+++ src/components/landing/landing-route.ts
@@ -2,9 +2,14 @@ import * as express from "express";
 import { landingGet } from "./landing-controller";
 import { PATH_NAMES } from "../../app.constants";
 import { validateSessionMiddleware } from "../../middleware/session-middleware";
+import { asyncHandler } from "../../utils/async";
 
 const router = express.Router();
 
-router.get(PATH_NAMES.ROOT, validateSessionMiddleware, landingGet);
+router.get(
+  PATH_NAMES.ROOT,
+  validateSessionMiddleware,
+  asyncHandler(landingGet)
+);
 
 export { router as landingRouter };
