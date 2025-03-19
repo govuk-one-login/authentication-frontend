547363717b4406377bf7212a99d985719e5d2df3
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2025-03-19 17:21:30 +0000

    AUT-4169: Do not log on healthcheck requests
    These are very noisy and not valuable

--- src/components/healthcheck/healthcheck-controller.ts
+++ src/components/healthcheck/healthcheck-controller.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from "express";
 import { HTTP_STATUS_CODES } from "../../app.constants";
-import { logger } from "../../utils/logger";
 
 export function healthcheckGet(req: Request, res: Response): void {
-  logger.info(`Healthcheck returning 200 OK.`);
   res.status(HTTP_STATUS_CODES.OK).send("OK");
 }
