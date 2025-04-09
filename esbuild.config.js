daf8978734f833338ca1b69ba6ab90321d58e5eb
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2025-04-09 09:52:22 +0100

    esbuild

---
+++ esbuild.config.js
@@ -0,0 +1,27 @@
+import esbuild from "esbuild";
+import path from "path";
+
+esbuild
+  .build({
+    entryPoints: ["./src/**/*.ts"],
+    outdir: "dist",
+    format: "esm",
+    platform: "node",
+    target: "es2020",
+    sourcemap: true,
+    bundle: false,
+    splitting: false,
+    outExtension: { ".js": ".js" },
+    loader: {
+      // TODO needed?
+      ".json": "json",
+    },
+    absWorkingDir: path.resolve(import.meta.dirname),
+  })
+  .then(() => {
+    console.log("Build complete");
+    if (process.argv.includes("--watch")) {
+      console.log("Watching for changes...");
+    }
+  })
+  .catch(() => process.exit(1));
