2a27d0c7c9e2bb7f0827e6a87e7b5bd15024585b
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2025-04-30 11:49:25 +0100

    AUT-1416: Include MFA reset link
    Users need to be able to reset their MFA if neither presented MFA option is viable for them. So here we add the link to MFA reset with IPV.

--- src/components/how-do-you-want-security-codes/how-do-you-want-security-codes-controller.ts
+++ src/components/how-do-you-want-security-codes/how-do-you-want-security-codes-controller.ts
@@ -1,8 +1,11 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
+import { PATH_NAMES } from "../../app.constants.js";
 
 export function howDoYouWantSecurityCodesGet(
   req: Request,
   res: Response
 ): void {
-  res.render("how-do-you-want-security-codes/index.njk");
+  res.render("how-do-you-want-security-codes/index.njk", {
+    mfaResetLink: PATH_NAMES.MFA_RESET_WITH_IPV,
+  });
 }
--- src/components/how-do-you-want-security-codes/tests/how-do-you-want-security-codes-integration.test.ts
+++ src/components/how-do-you-want-security-codes/tests/how-do-you-want-security-codes-integration.test.ts
@@ -1,11 +1,12 @@
 import { describe } from "mocha";
-import { request, sinon } from "../../../../test/utils/test-utils.js";
+import { expect, request, sinon } from "../../../../test/utils/test-utils.js";
 import nock from "nock";
 import { PATH_NAMES } from "../../../app.constants.js";
 import esmock from "esmock";
 import type { NextFunction, Request, Response } from "express";
 import { buildMfaMethods } from "../../../../test/helpers/mfa-helper.js";
 import { getPermittedJourneyForPath } from "../../../../test/helpers/session-helper.js";
+import * as cheerio from "cheerio";
 
 describe("Integration::how do you want security codes", () => {
   let app: any;
@@ -56,9 +57,24 @@ describe("Integration::how do you want security codes", () => {
     app = undefined;
   });
 
-  it("should return how do you want security codes page", (done) => {
-    request(app, (test) =>
-      test.get(PATH_NAMES.HOW_DO_YOU_WANT_SECURITY_CODES).expect(200, done)
+  it("should return how do you want security codes page as expected", async () => {
+    await request(app, (test) =>
+      test
+        .get(PATH_NAMES.HOW_DO_YOU_WANT_SECURITY_CODES)
+        .expect(200)
+        .expect(function (res) {
+          const $ = cheerio.load(res.text);
+          expect(
+            $("a")
+              .toArray()
+              .some(
+                (link) =>
+                  $(link).attr("href") === PATH_NAMES.MFA_RESET_WITH_IPV &&
+                  $(link).text().trim() ===
+                    "check if you can change how you get security codes"
+              )
+          ).to.be.eq(true, "mfa reset link presence");
+        })
     );
   });
 });
