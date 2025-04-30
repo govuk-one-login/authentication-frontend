7a9e64db276ba1df2ef5f8985e8326b9bba89774
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2025-04-30 08:51:28 +0100

    AUT-1416: Restrict access to the HOW_DO_YOU_WANT_SECURITY_CODES
    Only authenticated users on the enter-mfa page should be able to access the HOW_DO_YOU_WANT_SECURITY_CODES page.

--- src/components/how-do-you-want-security-codes/how-do-you-want-security-codes-routes.ts
+++ src/components/how-do-you-want-security-codes/how-do-you-want-security-codes-routes.ts
@@ -1,11 +1,15 @@
 import * as express from "express";
 import { PATH_NAMES } from "../../app.constants.js";
 import { howDoYouWantSecurityCodesGet } from "./how-do-you-want-security-codes-controller.js";
+import { validateSessionMiddleware } from "../../middleware/session-middleware.js";
+import { allowUserJourneyMiddleware } from "../../middleware/allow-user-journey-middleware.js";
 
 const router = express.Router();
 
 router.get(
   PATH_NAMES.HOW_DO_YOU_WANT_SECURITY_CODES,
+  validateSessionMiddleware,
+  allowUserJourneyMiddleware,
   howDoYouWantSecurityCodesGet
 );
 
--- src/components/how-do-you-want-security-codes/tests/how-do-you-want-security-codes-integration.test.ts
+++ src/components/how-do-you-want-security-codes/tests/how-do-you-want-security-codes-integration.test.ts
@@ -2,12 +2,49 @@ import { describe } from "mocha";
 import { request, sinon } from "../../../../test/utils/test-utils.js";
 import nock from "nock";
 import { PATH_NAMES } from "../../../app.constants.js";
+import esmock from "esmock";
+import type { NextFunction, Request, Response } from "express";
+import { buildMfaMethods } from "../../../../test/helpers/mfa-helper.js";
+import { getPermittedJourneyForPath } from "../../../../test/helpers/session-helper.js";
 
 describe("Integration::how do you want security codes", () => {
   let app: any;
+  const DEFAULT_PHONE_NUMBER = "7867";
+  const BACKUP_PHONE_NUMBER = "1234";
 
   before(async () => {
-    app = await (await import("../../../app.js")).createApp();
+    const { createApp } = await esmock(
+      "../../../app.js",
+      {},
+      {
+        "../../../middleware/session-middleware.js": {
+          validateSessionMiddleware: sinon.fake(function (
+            req: Request,
+            res: Response,
+            next: NextFunction
+          ): void {
+            res.locals.sessionId = "tDy103saszhcxbQq0-mjdzU854";
+
+            req.session.user = {
+              email: "test@test.com",
+              mfaMethods: buildMfaMethods([
+                {
+                  redactedPhoneNumber: DEFAULT_PHONE_NUMBER,
+                },
+                {
+                  redactedPhoneNumber: BACKUP_PHONE_NUMBER,
+                },
+              ]),
+              journey: getPermittedJourneyForPath(PATH_NAMES.ENTER_MFA),
+              isAccountRecoveryPermitted: true,
+            };
+            next();
+          }),
+        },
+      }
+    );
+
+    app = await createApp();
   });
 
   beforeEach(() => {
