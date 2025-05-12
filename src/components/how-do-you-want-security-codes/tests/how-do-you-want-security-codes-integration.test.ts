3adf8c8d04ff7a5ac0464ddf6f262566ae870ec8
Aidan Comer <alhcomer@hotmail.co.uk>
2025-05-12 15:16:50 +0100

    AUT-4246: Add auth app to how do you want security codes
    - Adds logic for auth app to the how do you want to get security codes page.
    - If auth app is in present in req.session.user.mfaMethods, auth app radio button
      is shown.

--- src/components/how-do-you-want-security-codes/tests/how-do-you-want-security-codes-integration.test.ts
+++ src/components/how-do-you-want-security-codes/tests/how-do-you-want-security-codes-integration.test.ts
@@ -30,8 +30,18 @@ describe("Integration::how do you want security codes", () => {
   const DEFAULT_PHONE_NUMBER_ID = "532b14c2-a11d-4882-a83b-e8d7184e0b70";
   const BACKUP_PHONE_NUMBER = "1234";
   const BACKUP_PHONE_NUMBER_ID = "6ae3be91-708f-45b2-9374-6a595eb76bce";
+  const BACKUP_AUTH_APP_ID = "8f40b828-2928-492f-a277-8432d9e76d2a";
 
-  before(async () => {
+  beforeEach(() => {
+    nock.cleanAll();
+  });
+
+  after(() => {
+    sinon.restore();
+    app = undefined;
+  });
+
+  it("should return how do you want security codes page as expected for SMS user with SMS backup", async () => {
     const { createApp } = await esmock(
       "../../../app.js",
       {},
@@ -66,18 +76,7 @@ describe("Integration::how do you want security codes", () => {
     );
 
     app = await createApp();
-  });
 
-  beforeEach(() => {
-    nock.cleanAll();
-  });
-
-  after(() => {
-    sinon.restore();
-    app = undefined;
-  });
-
-  it("should return how do you want security codes page as expected", async () => {
     await request(app, (test) =>
       test
         .get(PATH_NAMES.HOW_DO_YOU_WANT_SECURITY_CODES)
@@ -119,7 +118,119 @@ describe("Integration::how do you want security codes", () => {
     );
   });
 
+  it("should return how do you want security codes page as expected for SMS user with AUTH APP backup", async () => {
+    const { createApp } = await esmock(
+      "../../../app.js",
+      {},
+      {
+        "../../../middleware/session-middleware.js": {
+          validateSessionMiddleware: sinon.fake(function (
+            req: Request,
+            res: Response,
+            next: NextFunction
+          ): void {
+            res.locals.sessionId = "tDy103saszhcxbQq0-mjdzU854";
+
+            req.session.user = {
+              email: "test@test.com",
+              mfaMethods: buildMfaMethods([
+                {
+                  id: DEFAULT_PHONE_NUMBER_ID,
+                  redactedPhoneNumber: DEFAULT_PHONE_NUMBER,
+                },
+                {
+                  id: BACKUP_AUTH_APP_ID,
+                  authApp: true,
+                },
+              ]),
+              journey: getPermittedJourneyForPath(PATH_NAMES.ENTER_MFA),
+              isAccountRecoveryPermitted: true,
+            };
+            next();
+          }),
+        },
+      }
+    );
+
+    app = await createApp();
+
+    await request(app, (test) =>
+      test
+        .get(PATH_NAMES.HOW_DO_YOU_WANT_SECURITY_CODES)
+        .expect(200)
+        .expect(function (res) {
+          const $ = cheerio.load(res.text);
+          expect(
+            $("a")
+              .toArray()
+              .some(
+                (link) =>
+                  $(link).attr("href") === PATH_NAMES.MFA_RESET_WITH_IPV &&
+                  $(link).text().trim() ===
+                    "check if you can change how you get security codes"
+              )
+          ).to.be.eq(true, "mfa reset link presence");
+
+          const form = $(`form[action="/how-do-you-want-security-codes"]`);
+          expect(form.toArray().some(Boolean)).to.be.eq(true, "form presence");
+          expect(
+            form
+              .first()
+              .find("button[type=Submit]")
+              .toArray()
+              .some((link) => $(link).text().trim() === "Continue")
+          ).to.be.eq(true, "submit button presence");
+
+          const radioArray = form.first().find("input[type=radio]").toArray();
+          expect(radioArray.length).to.be.eq(2);
+          expect($(radioArray[0]).val() === BACKUP_AUTH_APP_ID).to.be.eq(
+            true,
+            `radio input presence for auth app in correct order`
+          );
+          expect($(radioArray[1]).val() === DEFAULT_PHONE_NUMBER_ID).to.be.eq(
+            true,
+            `radio input presence for ${DEFAULT_PHONE_NUMBER} in correct order`
+          );
+        })
+    );
+  });
+
   it("returns a validation error when no option is selected", async () => {
+    const { createApp } = await esmock(
+      "../../../app.js",
+      {},
+      {
+        "../../../middleware/session-middleware.js": {
+          validateSessionMiddleware: sinon.fake(function (
+            req: Request,
+            res: Response,
+            next: NextFunction
+          ): void {
+            res.locals.sessionId = "tDy103saszhcxbQq0-mjdzU854";
+
+            req.session.user = {
+              email: "test@test.com",
+              mfaMethods: buildMfaMethods([
+                {
+                  id: DEFAULT_PHONE_NUMBER_ID,
+                  redactedPhoneNumber: DEFAULT_PHONE_NUMBER,
+                },
+                {
+                  id: BACKUP_PHONE_NUMBER_ID,
+                  redactedPhoneNumber: BACKUP_PHONE_NUMBER,
+                },
+              ]),
+              journey: getPermittedJourneyForPath(PATH_NAMES.ENTER_MFA),
+              isAccountRecoveryPermitted: true,
+            };
+            next();
+          }),
+        },
+      }
+    );
+
+    app = await createApp();
+
     const { token, cookies } = await getTokenAndCookies(app);
 
     await request(
