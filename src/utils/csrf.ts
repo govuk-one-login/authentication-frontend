e409fb75d5a58a415f0cde95fd2b6b1531f49f69
Matt W <173997080+sw-mattw@users.noreply.github.com>
2025-05-29 09:47:25 +0100

    AUT-4272: skip csrf protection on all routes
    Note that this will be re-enabled as part of the third PR under this ticket. We want to ensure the csrf token is being set on the session and injected into the form hidden input before we start enforcing protection. This is with the aim of ensuring partially completed user journeys (with an active session) are not affected during the deployment (which would otherwise fail due to a "csrf error", e.g. if no token was in the hidden form input at the time of submit).

--- src/utils/csrf.ts
+++ src/utils/csrf.ts
@@ -4,6 +4,10 @@ const { csrfSynchronisedProtection } = csrfSync({
   getTokenFromRequest: (req) => {
     return req.body["_csrf"];
   },
+  // TODO: AUT-4272 PR 3: Remove this to re-enable csrf validation.
+  skipCsrfProtection: (req) => {
+    return true;
+  },
 });
 
 export { csrfSynchronisedProtection };
