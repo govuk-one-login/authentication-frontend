91f499c0ef59c7196687bce41d2ccf5c4ff43e60
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2025-06-10 09:29:37 +0100

    BAU: add header with optional journey toggle

--- journey-map/src/index.ts
+++ journey-map/src/index.ts
@@ -1,6 +1,6 @@
 import mermaid from "mermaid";
 import svgPanZoom from "svg-pan-zoom";
-import { renderStateMachine } from "./mermaid.js";
+import { Options, renderStateMachine } from "./mermaid.js";
 
 declare global {
   interface Window {
@@ -10,11 +10,30 @@ declare global {
 }
 
 const diagramElement = document.getElementById("diagram") as HTMLDivElement;
+const headerContent = document.getElementById(
+  "header-content",
+) as HTMLDivElement;
+const headerToggle = document.getElementById(
+  "header-toggle",
+) as HTMLButtonElement;
+const form = document.getElementById("configuration-form") as HTMLFormElement;
+
+const setupHeaderToggleClickHandlers = (): void => {
+  headerToggle.addEventListener("click", () => {
+    if (headerContent.classList.toggle("hidden")) {
+      headerToggle.innerText = "Show header";
+    } else {
+      headerToggle.innerText = "Hide header";
+    }
+  });
+};
+
+const parseOptions = (formData: FormData): Options => ({
+  includeOptional: formData.getAll("otherOption").includes("includeOptional"),
+});
 
 const render = async (): Promise<void> => {
-  const options = {
-    includeOptional: false,
-  };
+  const options = parseOptions(new FormData(form));
   const stateMachineMermaid = renderStateMachine(options);
   console.log(stateMachineMermaid);
 
@@ -38,7 +57,8 @@ const render = async (): Promise<void> => {
   });
 };
 
-const highlightState = (state: string): void => {
+const setupStateClickHandlers = (): void => {
+  const highlightState = (state: string): void => {
   // Remove existing highlights
   Array.from(document.getElementsByClassName("highlight")).forEach((edge) =>
     edge.classList.remove("highlight", "outgoingEdge", "incomingEdge")
@@ -54,10 +74,21 @@ const highlightState = (state: string): void => {
   Array.from(document.getElementsByClassName("node"))
     .filter((node) => node.id.startsWith(`flowchart-${state}-`))
     .forEach((node) => node.classList.add("highlight"));
+  };
+
+  window.onStateClick = async (state: string): Promise<void> => {
+    highlightState(state);
+  };
+}
+
+const initialise = async (): Promise<void> => {
+  setupHeaderToggleClickHandlers();
+  setupStateClickHandlers();
+  render();
+  form.addEventListener("change", async (event) => {
+    event.preventDefault();
+    await render();
+  });
 };
 
-window.onStateClick = async (state: string): Promise<void> => {
-  highlightState(state);
-};
-
-render().catch(console.error);
+initialise().catch(console.error);
