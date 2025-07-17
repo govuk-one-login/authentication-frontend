c0542530cb30fa23ef93463de6c028b1a4a4fd3e
Benjamin Fernandes <benjamin.fernandes@digital.cabinet-office.gov.uk>
2025-07-17 01:16:39 +0100

    BAU: Replace HTML snapshots with JPEGs

--- src/components/contact-us/tests/contact-us-snapshot.test.ts
+++ src/components/contact-us/tests/contact-us-snapshot.test.ts
@@ -1,100 +1,75 @@
-import { describe } from "mocha";
-import { CONTACT_FORM_STRUCTURE, PATH_NAMES } from "../../../app.constants.js";
+import { test, expect } from "@playwright/test";
+import { CONTACT_FORM_STRUCTURE } from "../../../app.constants.js";
 import type { Theme } from "src/app.constants.js";
-import { request, sinon } from "../../../../test/utils/test-utils.js";
-import { expect } from "chai";
-import type { NextFunction, Request, Response } from "express";
-import esmock from "esmock";
 
-describe("Snapshot:: contact us - public user", () => {
-  let app: Express.Application;
+/*
+TODO
+- Extract stuff below to make reusable
+- Trigger errors
+- Test different languages
+ */
 
-  before(async () => {
-    const { createApp } = await esmock(
-      "../../../app.js",
-      {},
-      {
-        "../../../middleware/csrf-middleware.js": {
-          csrfMiddleware: sinon.fake(function (
-            _req: Request,
-            res: Response,
-            next: NextFunction
-          ) {
-            res.locals.csrfToken = "TEST_CSRF_TOKEN";
-            next();
-          }),
-        },
-        "../../../middleware/set-local-vars-middleware.js": {
-          setLocalVarsMiddleware: sinon.fake(function (
-            _req: Request,
-            res: Response,
-            next: NextFunction
-          ) {
-            res.locals.scriptNonce = "TEST_NONCE";
-            next();
-          }),
-        },
-        "../../../middleware/outbound-contact-us-links-middleware.js": {
-          outboundContactUsLinksMiddleware: sinon.fake(function (
-            _req: Request,
-            res: Response,
-            next: NextFunction
-          ) {
-            res.locals.contactUsLinkUrl = "https://example.com";
-            next();
-          }),
-        },
-      }
-    );
-    app = await createApp();
-  });
-
-  it("should render /contact-us", async () => {
-    const res = await request(app, (test) => test.get(PATH_NAMES.CONTACT_US), {
-      expectAnalyticsPropertiesMatchSnapshot: false,
+test.describe.parallel("Snapshot:: contact us - public user", () => {
+  const contactUsPath = "/contact-us";
+  test(`should render ${contactUsPath}`, async ({ page }) => {
+    await page.goto(contactUsPath);
+    const actualScreenshot = await page.screenshot({
+      fullPage: true,
+      type: "jpeg",
+      quality: 20,
     });
-    expect(res.text).toMatchSnapshot();
+    expect(actualScreenshot).toMatchSnapshot(`contact-us--en.jpeg`, {
+      threshold: 0.25,
+    });
   });
 
   CONTACT_FORM_STRUCTURE.forEach((theme: Theme, themeKey: string) => {
-    describe(themeKey, () => {
+    test.describe.parallel(themeKey, () => {
       if (theme.subThemes) {
-        it(`should render /contact-us-further-information?theme=${themeKey}`, async () => {
-          const res = await request(
-            app,
-            (test) =>
-              test
-                .get(PATH_NAMES.CONTACT_US_FURTHER_INFORMATION)
-                .query({ theme: themeKey }),
-            { expectAnalyticsPropertiesMatchSnapshot: false }
+        const subThemeFurtherInformationPath = `/contact-us-further-information?theme=${themeKey}`;
+        test(`should render ${subThemeFurtherInformationPath}`, async ({
+          page,
+        }) => {
+          await page.goto(subThemeFurtherInformationPath);
+          const actualScreenshot = await page.screenshot({
+            fullPage: true,
+            type: "jpeg",
+            quality: 20,
+          });
+          expect(actualScreenshot).toMatchSnapshot(
+            `contact-us-further-information--en--${themeKey}.jpeg`,
+            { threshold: 0.25 }
           );
-          expect(res.text).toMatchSnapshot();
         });
 
         theme.subThemes.forEach((_subTheme: Theme, subThemeKey) => {
-          it(`should render /contact-us-questions?theme=${themeKey}&subtheme=${subThemeKey}`, async () => {
-            const res = await request(
-              app,
-              (test) =>
-                test
-                  .get(PATH_NAMES.CONTACT_US_QUESTIONS)
-                  .query({ theme: themeKey, subtheme: subThemeKey }),
-              { expectAnalyticsPropertiesMatchSnapshot: false }
+          const subThemeQuestionsPath = `/contact-us-questions?theme=${themeKey}&subtheme=${subThemeKey}`;
+          test(`should render ${subThemeQuestionsPath}`, async ({ page }) => {
+            await page.goto(subThemeQuestionsPath);
+            const actualScreenshot = await page.screenshot({
+              fullPage: true,
+              type: "jpeg",
+              quality: 20,
+            });
+            expect(actualScreenshot).toMatchSnapshot(
+              `contact-us-questions--en--${themeKey}--${subThemeKey}.jpeg`,
+              { threshold: 0.25 }
             );
-            expect(res.text).toMatchSnapshot();
           });
         });
       } else {
-        it(`should render /contact-us-questions?theme=${themeKey}`, async () => {
-          const res = await request(
-            app,
-            (test) =>
-              test
-                .get(PATH_NAMES.CONTACT_US_FURTHER_INFORMATION)
-                .query({ theme: themeKey }),
-            { expectAnalyticsPropertiesMatchSnapshot: false }
+        const themeQuestionsPath = `/contact-us-questions?theme=${themeKey}`;
+        test(`should render ${themeQuestionsPath}`, async ({ page }) => {
+          await page.goto(themeQuestionsPath);
+          const actualScreenshot = await page.screenshot({
+            fullPage: true,
+            type: "jpeg",
+            quality: 20,
+          });
+          expect(actualScreenshot).toMatchSnapshot(
+            `contact-us-questions--en--${themeKey}.jpeg`,
+            { threshold: 0.25 }
           );
-          expect(res.text).toMatchSnapshot();
         });
       }
     });
