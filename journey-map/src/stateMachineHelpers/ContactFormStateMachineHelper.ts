43d44c0ed3f93c558a45b228a628ae7d03b6f879
Benjamin Fernandes <benjamin.fernandes@digital.cabinet-office.gov.uk>
2025-10-01 16:13:45 +0100

    AUT-4766: Add contact form submit success to state machine

--- journey-map/src/stateMachineHelpers/ContactFormStateMachineHelper.ts
+++ journey-map/src/stateMachineHelpers/ContactFormStateMachineHelper.ts
@@ -6,6 +6,9 @@ import {
 import i18next from "i18next";
 
 export default class ContactFormStateMachineHelper extends StateMachineHelper {
+  private readonly BASE_ID = "contact-us";
+  private readonly SUBMIT_SUCCESS_ID = `${this.BASE_ID}-submit-success`;
+
   getReachableStatesAndTransitions(): {
     states: State[];
     transitions: Transition[];
@@ -13,35 +16,49 @@ export default class ContactFormStateMachineHelper extends StateMachineHelper {
     const states: State[] = [];
     const transitions: Transition[] = [];
 
-    const baseId = "contact-us";
-
     states.push({
       name: "Contact Us",
-      id: baseId,
+      id: this.BASE_ID,
+    });
+
+    states.push({
+      name: "Submit Success",
+      id: this.SUBMIT_SUCCESS_ID,
     });
 
     CONTACT_FORM_STRUCTURE.forEach((theme, themeKey) => {
-      const themeId = `${baseId}.${themeKey}`.replaceAll("_", "-");
+      const themeId = `${this.BASE_ID}.${themeKey}`.replaceAll("_", "-");
       states.push({
         name: i18next.t(`${theme.nextPageContent}.title`),
         id: themeId,
       });
       transitions.push({
-        source: baseId,
+        source: this.BASE_ID,
         target: themeId,
       });
 
-      theme.subThemes?.forEach((subTheme, subThemeKey) => {
-        const subThemeId = `${themeId}.${subThemeKey}`.replaceAll("_", "-");
-        states.push({
-          name: i18next.t(`${subTheme.nextPageContent}.title`),
-          id: subThemeId,
+      if (theme.subThemes) {
+        theme.subThemes.forEach((subTheme, subThemeKey) => {
+          const subThemeId = `${themeId}.${subThemeKey}`.replaceAll("_", "-");
+          states.push({
+            name: i18next.t(`${subTheme.nextPageContent}.title`),
+            id: subThemeId,
+          });
+          transitions.push({
+            source: themeId,
+            target: subThemeId,
+          });
+          transitions.push({
+            source: subThemeId,
+            target: this.SUBMIT_SUCCESS_ID,
+          });
         });
+      } else {
         transitions.push({
           source: themeId,
-          target: subThemeId,
+          target: this.SUBMIT_SUCCESS_ID,
         });
-      });
+      }
     });
 
     return {
@@ -52,22 +69,29 @@ export default class ContactFormStateMachineHelper extends StateMachineHelper {
 
   getClickAction(state: State): (() => void) | undefined {
     return () => {
-      const pathParts = state.id.split(".");
+      const idParts = state.id.split(".");
 
       let path;
-      if (pathParts.length === 1) {
-        path = "/contact-us";
-      } else if (pathParts.length === 2) {
-        const themeKey = pathParts[1].replaceAll("-", "_");
+      if (idParts.length === 1) {
+        switch (idParts[0]) {
+          case this.BASE_ID:
+            path = "/contact-us";
+            break;
+          case this.SUBMIT_SUCCESS_ID:
+            path = "/contact-us-submit-success";
+            break;
+        }
+      } else if (idParts.length === 2) {
+        const themeKey = idParts[1].replaceAll("-", "_");
         const theme = CONTACT_FORM_STRUCTURE.get(themeKey) as Theme;
         if (theme.subThemes) {
           path = `/contact-us-further-information?theme=${themeKey}`;
         } else {
           path = `/contact-us-questions?theme=${themeKey}`;
         }
-      } else if (pathParts.length === 3) {
-        const themeKey = pathParts[1].replaceAll("-", "_");
-        const subThemeKey = pathParts[2].replaceAll("-", "_");
+      } else if (idParts.length === 3) {
+        const themeKey = idParts[1].replaceAll("-", "_");
+        const subThemeKey = idParts[2].replaceAll("-", "_");
         path = `/contact-us-questions?theme=${themeKey}&subtheme=${subThemeKey}`;
       }
 
