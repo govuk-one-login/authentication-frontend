e4b92931b92d002e7ba522bc3aa043c4af9fea44
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2025-10-21 18:10:27 +0100

    BAU: Add standard session and route validation
    Email blocks error page should only be accessible when the state machine says it should be and the user should also have a valid session at this point

--- src/components/cannot-use-email-address/cannot-use-email-address-routes.ts
+++ src/components/cannot-use-email-address/cannot-use-email-address-routes.ts
@@ -4,13 +4,22 @@ import {
   cannotUseEmailAddressContinueGet,
   cannotUseEmailAddressGet,
 } from "./cannot-use-email-address-controller.js";
+import { allowUserJourneyMiddleware } from "../../middleware/allow-user-journey-middleware.js";
+import { validateSessionMiddleware } from "../../middleware/session-middleware.js";
 
 const router = express.Router();
 
-router.get(PATH_NAMES.CANNOT_USE_EMAIL_ADDRESS, cannotUseEmailAddressGet);
+router.get(
+  PATH_NAMES.CANNOT_USE_EMAIL_ADDRESS,
+  validateSessionMiddleware,
+  allowUserJourneyMiddleware,
+  cannotUseEmailAddressGet
+);
 
 router.get(
   PATH_NAMES.CANNOT_USE_EMAIL_ADDRESS_CONTINUE,
+  validateSessionMiddleware,
+  allowUserJourneyMiddleware,
   cannotUseEmailAddressContinueGet
 );
 
