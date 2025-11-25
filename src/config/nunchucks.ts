622d4b0186f4cc3550419bbed5bcd5236f9be135
Matt W <173997080+sw-mattw@users.noreply.github.com>
2025-11-25 12:00:16 +0000

    add debug

--- src/config/nunchucks.ts
+++ src/config/nunchucks.ts
@@ -5,6 +5,7 @@ import { returnLastCharactersOnly } from "../utils/phone-number.js";
 import { getNodeEnv, useRebrand } from "../config.js";
 import { ENVIRONMENT_NAME } from "../app.constants.js";
 import addLanguageParam from "@govuk-one-login/frontend-language-toggle";
+import { logger } from "../utils/logger.js";
 
 interface FilterContext {
   ctx: {
@@ -28,8 +29,17 @@ export function configureNunjucks(
   nunjucksEnv.addFilter(
     "translate",
     function (this: FilterContext, key: string, options?: any) {
-      const language = this.ctx.i18n?.language || this.ctx.htmlLang || 'en';
+      // const language = this.ctx.i18n?.language || this.ctx.htmlLang || i18next.resolvedLanguage || 'en';
+
+      const ctxI18nLang = this.ctx.i18n?.language;
+      const ctxHtmlLang = this.ctx.htmlLang;
+      const i18nextResolved = i18next.resolvedLanguage;
+      const language = ctxI18nLang || ctxHtmlLang || i18nextResolved || 'en';
+
+      logger.debug({ ctxI18nLang, ctxHtmlLang, i18nextResolved, language, key }, 'translate filter');
+
       const translate = i18next.getFixedT(language);
+
       return translate(key, options);
     }
   );
