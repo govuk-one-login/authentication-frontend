c169fd9e46ff9c6f463b3fa1590bacd34b331657
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2025-11-27 16:30:36 +0000

    WIP for running everything locally

--- startup.sh
+++ startup.sh
@@ -12,14 +12,18 @@ function usage() {
   echo "Usage: startup.sh [-c] [-l]" >&2
   echo "  -c: Clean dist and node_modules" >&2
   echo "  -l: Start frontend natively (not in docker)" >&2
+  echo "  -L: Start frontend natively (not in docker) with local running backend" >&2
   echo "  -x: Only start dependencies (redis, stubs)" >&2
   exit 1
 }
 
-while getopts ":clx" opt; do
+while getopts ":clLx" opt; do
   case ${opt} in
   l)
     ACTION_LOCAL=1
+    ;;
+  L)
+    ACTION_FULL_LOCAL=1
     ;;
   c)
     ACTION_CLEAN=1
@@ -42,7 +46,7 @@ if [ ${#} -gt 0 ]; then
   usage "No extra arguments allowed. Got: ${*}"
 fi
 
-if [ "${ACTION_LOCAL:-0}" == "0" ]; then
+if [ "${ACTION_LOCAL:-0}" == "0" ] && [ "${ACTION_FULL_LOCAL:-0}" == "0" ]; then
   echo "WARNING: Running frontend in docker. This CAN be a bit flaky. If you encounter issues, try running natively with \`startup.sh -l\`"
   for ((n = 0; n < 3; n++)); do
     echo -n "."
@@ -60,14 +64,27 @@ fi
 
 "${DIR}"/shutdown.sh
 
-test -f .env || usage "Missing .env file"
+if [ "${ACTION_FULL_LOCAL:-0}" == "1" ]; then
+  test -f .env.local || usage "Missing .env.local file"
+else
+  test -f .env || usage "Missing .env file"
+fi
+
+
 
 # shellcheck source=/dev/null
-set -o allexport && source .env && set +o allexport
+if [ "${ACTION_FULL_LOCAL:-0}" == "1" ]; then
+  echo "exporting env vars from .env.local"
+  set -o allexport && source .env.local && set +o allexport
+else
+  echo "exporting env vars from .env"
+  set -o allexport && source .env && set +o allexport
+fi
 
 # shellcheck source=./scripts/export_aws_creds.sh
-source "${DIR}/scripts/export_aws_creds.sh"
-unset AWS_PROFILE
+#source "${DIR}/scripts/export_aws_creds.sh"
+
+#unset AWS_PROFILE
 
 if [ "${ACTION_LOCAL:-0}" == "1" ]; then
   echo "Starting frontend local service..."
@@ -81,12 +98,26 @@ if [ "${ACTION_LOCAL:-0}" == "1" ]; then
   else
     docker compose -f docker-compose.yml logs -f
   fi
+elif [ "${ACTION_FULL_LOCAL:-0}" == "1" ]; then
+  echo "Starting frontend local service and backend local service..."
+  (
+    cd "${DIR}"
+    cd ..
+    cd authentication-api/local-running
+    docker compose up --build -d authentication-api orchestration-stub
+  )
+  echo "Will listen on redis port ${REDIS_PORT:-6380}"
+  docker compose -f docker-compose.yml up --build -d --wait
+  export REDIS_PORT=${REDIS_PORT:-6380}
+  export REDIS_HOST=localhost
+  echo "Redis listening on redis://localhost:${REDIS_PORT:-6379}"
+  export PORT="${DOCKER_FRONTEND_PORT:-3000}"
+  yarn install && yarn copy-assets && yarn dev
 else
   echo "Starting frontend service..."
   docker compose -f docker-compose.yml -f docker-compose.frontend.yml up -d --wait --build
   echo "Redis listening on redis://localhost:${REDIS_PORT:-6379}"
   echo "Frontend listening on http://localhost:${DOCKER_FRONTEND_PORT:-3000}"
   echo "Frontend nodemon listening on localhost:${DOCKER_FRONTEND_NODEMON_PORT:-9230}"
-
   docker compose -f docker-compose.yml -f docker-compose.frontend.yml logs -f
 fi
