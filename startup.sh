431c119c6efd1e1bc43e4fb8c3df5a42c31836ea
Aidan Comer <alhcomer@hotmail.co.uk>
2025-11-27 07:36:53 +0000

    BAU: Add esbuild dependency
    - tsx requires esbuild
    - Previously, it was installed by install scripts
    - Since scripts are now ignored, we now need to include manually

--- startup.sh
+++ startup.sh
@@ -80,7 +80,9 @@ if [ "${ACTION_LOCAL:-0}" == "1" ]; then
   echo "Redis listening on redis://localhost:${REDIS_PORT:-6379}"
   if [ "${ACTION_DEPS_ONLY:-0}" == "0" ]; then
     export PORT="${DOCKER_FRONTEND_PORT:-3000}"
-    npm ci && npm run test:dev-evironment-variables && npm run copy-assets && npm run dev
+    npm ci
+    node node_modules/esbuild/install.js
+    npm run test:dev-evironment-variables && npm run copy-assets && npm run dev
   else
     docker compose -f docker-compose.yml logs -f
   fi
