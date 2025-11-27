d9a145611ec24a15742c7cb77e960e462dc92e11
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2025-11-27 16:30:36 +0000

    BAU: Run frontend in a local node instance against the backend running in Docker
    - Add a new flag -L to the startup.sh script
    - Add a new if block for this flag to startup the backend locally and the frontend in a node instance
    - Add a new section to the README for this change

--- startup.sh
+++ startup.sh
@@ -12,14 +12,18 @@ function usage() {
   echo "Usage: startup.sh [-c] [-l]" >&2
   echo "  -c: Clean dist and node_modules" >&2
   echo "  -l: Start frontend natively (not in docker)" >&2
+  echo "  -L: Start frontend natively (not in docker) with local running backend" >&2
   echo "  -x: Only start dependencies (redis, stubs)" >&2
   exit 1
 }
 
-while getopts ":clx" opt; do
+while getopts ":clLx" opt; do
   case ${opt} in
   l)
     ACTION_LOCAL=1
+    ;;
+  L)
+    ACTION_FULL_LOCAL=1
     ;;
   c)
     ACTION_CLEAN=1
@@ -42,7 +46,7 @@ if [ ${#} -gt 0 ]; then
   usage "No extra arguments allowed. Got: ${*}"
 fi
 
-if [ "${ACTION_LOCAL:-0}" == "0" ]; then
+if [ "${ACTION_LOCAL:-0}" == "0" ] && [ "${ACTION_FULL_LOCAL:-0}" == "0" ]; then
   echo "WARNING: Running frontend in docker. This CAN be a bit flaky. If you encounter issues, try running natively with \`startup.sh -l\`"
   for ((n = 0; n < 3; n++)); do
     echo -n "."
@@ -60,14 +64,26 @@ fi
 
 "${DIR}"/shutdown.sh
 
-test -f .env || usage "Missing .env file"
+if [ "${ACTION_FULL_LOCAL:-0}" == "1" ]; then
+  test -f .env.local || usage "Missing .env.local file"
+else
+  test -f .env || usage "Missing .env file"
+fi
+
+
 
 # shellcheck source=/dev/null
-set -o allexport && source .env && set +o allexport
+if [ "${ACTION_FULL_LOCAL:-0}" == "1" ]; then
+  echo "exporting env vars from .env.local"
+  set -o allexport && source .env.local && set +o allexport
+else
+  echo "exporting env vars from .env"
+  set -o allexport && source .env && set +o allexport
+  source "${DIR}/scripts/export_aws_creds.sh"
+  unset AWS_PROFILE
+fi
 
 # shellcheck source=./scripts/export_aws_creds.sh
-source "${DIR}/scripts/export_aws_creds.sh"
-unset AWS_PROFILE
 
 # use .git-blame-ignore-revs to ignore blame on specified commits
 git config blame.ignoreRevsFile .git-blame-ignore-revs
@@ -92,12 +108,21 @@ if [ "${ACTION_LOCAL:-0}" == "1" ]; then
   else
     docker compose -f docker-compose.yml logs -f
   fi
+elif [ "${ACTION_FULL_LOCAL:-0}" == "1" ]; then
+  echo "Starting frontend local service and backend local service..."
+  (
+    cd "${DIR}"
+    cd ..
+    cd authentication-api/local-running
+    docker compose restart aws
+    docker compose up --build -d authentication-api orchestration-stub
+  )
+  npm run dev
 else
   echo "Starting frontend service..."
   docker compose -f docker-compose.yml -f docker-compose.frontend.yml up -d --wait --build
   echo "Redis listening on redis://localhost:${REDIS_PORT:-6379}"
   echo "Frontend listening on http://localhost:${DOCKER_FRONTEND_PORT:-3000}"
   echo "Frontend nodemon listening on localhost:${DOCKER_FRONTEND_NODEMON_PORT:-9230}"
-
   docker compose -f docker-compose.yml -f docker-compose.frontend.yml logs -f
 fi
