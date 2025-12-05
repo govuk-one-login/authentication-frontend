ba9329be2a3df9b7cb9f08a2b47d6ee634aca251
Aidan Comer <alhcomer@hotmail.co.uk>
2025-12-05 11:58:45 +0000

    BAU: refer to node:fs when importing
    This is preferred by SonarQube

--- playwright-acceptance-tests/src/support/hooks.ts
+++ playwright-acceptance-tests/src/support/hooks.ts
@@ -1,8 +1,8 @@
 import type { ITestCaseHookParameter } from "@cucumber/cucumber";
 import { Before, After, Status } from "@cucumber/cucumber";
 import type { PlaywrightWorld } from "./world";
-import fs from "fs";
-import path from "path";
+import fs from "node:fs";
+import path from "node:path";
 
 Before(async function (this: PlaywrightWorld) {
   await this.openBrowser();
