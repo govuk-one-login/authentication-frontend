3c6555f24cc8323273ea5cdc41e240b843f96b43
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-02-02 15:59:05 +0000

    ATO-2282: Add feature flag for using JWKS

--- src/config.ts
+++ src/config.ts
@@ -210,3 +210,7 @@ export function enableDwpKbvContactFormChanges(): boolean {
 export function supportNewInternationalSms(): boolean {
   return process.env.SUPPORT_NEW_INTERNATIONAL_SMS === "1";
 }
+
+export function getUseAuthJwks(): boolean {
+  return process.env.USE_AUTH_JWKS === "1";
+}
