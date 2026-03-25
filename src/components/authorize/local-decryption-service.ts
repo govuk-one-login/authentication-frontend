f6f76484c659f7e16ac52733db0759fd20db9a96
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-03-25 12:10:39 +0000

    BAU: Fix local running
    - Due to the major update from jose, it expects the decryption algorithm to look different (have hyphens instead of underscores)
    - Ensure copy-assets is run when we run local dev script so that updated nunjucks templates are used
    - Add another bullet point in the PR template to check that local running is still working

--- src/components/authorize/local-decryption-service.ts
+++ src/components/authorize/local-decryption-service.ts
@@ -6,7 +6,7 @@ const decoder = new TextDecoder();
 
 export class LocalDecryptionService implements DecryptionServiceInterface {
   async decrypt(serializedJwe: string): Promise<string> {
-    const key = await jose.importPKCS8(getLocalEncryptionKey(), "RSA_OAEP_256");
+    const key = await jose.importPKCS8(getLocalEncryptionKey(), "RSA-OAEP-256");
     const result = await jose.compactDecrypt(serializedJwe, key);
     return decoder.decode(result.plaintext);
   }
