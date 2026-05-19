365ef11c52175a92aa0f9753b1a624b56025568b
BeckaL <25515510+BeckaL@users.noreply.github.com>
2026-05-19 17:05:15 +0100

    AUT-4989: Submit button on account with passkey exists page routes to sign in with passkey
    Allow the onwards journey from the page the user lands on when they try to create an account with an email that already exists, associated with an account with a passkey

--- src/components/account-exists-with-passkey/tests/account-exists-with-passkey-controller-test.ts
+++ src/components/account-exists-with-passkey/tests/account-exists-with-passkey-controller-test.ts
@@ -6,7 +6,10 @@ import type { RequestOutput, ResponseOutput } from "mock-req-res";
 import { mockResponse } from "mock-req-res";
 import { PATH_NAMES } from "../../../app.constants.js";
 import { createMockRequest } from "../../../../test/helpers/mock-request-helper.js";
-import { accountExistsWithPasskeyGet } from "../account-exists-with-passkey-controller.js";
+import {
+  accountExistsWithPasskeyGet,
+  accountExistsWithPasskeyPost,
+} from "../account-exists-with-passkey-controller.js";
 import { commonVariables } from "../../../../test/helpers/common-test-variables.js";
 
 describe("account exists with passkey", () => {
@@ -33,6 +36,19 @@ describe("account exists with passkey", () => {
         "account-exists-with-passkey/index.njk",
         { email }
       );
+    });
+  });
+
+  describe("accountExistsWithPasskeyPost", () => {
+    it("should redirect to sign in with passkey", async () => {
+      req.session.user.journey = {
+        nextPath: PATH_NAMES.ACCOUNT_EXISTS_WITH_PASSKEY,
+        optionalPaths: [],
+      };
+
+      await accountExistsWithPasskeyPost()(req, res);
+
+      expect(res.redirect).to.have.calledWith(PATH_NAMES.SIGN_IN_WITH_PASSKEY);
     });
   });
 });
