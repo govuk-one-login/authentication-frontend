c8a687aac14f45004a2b039a5b32c148b4291149
BeckaL <25515510+BeckaL@users.noreply.github.com>
2026-05-19 17:05:02 +0100

    AUT-4989: Rename existing get route to be more consistent with others

--- src/components/account-exists-with-passkey/account-exists-with-passkey-routes.ts
+++ src/components/account-exists-with-passkey/account-exists-with-passkey-routes.ts
@@ -1,6 +1,6 @@
 import { PATH_NAMES } from "../../app.constants.js";
 import { Router } from "express";
-import { getAccountExistsWithPasskey } from "./account-exists-with-passkey-controller.js";
+import { accountExistsWithPasskeyGet } from "./account-exists-with-passkey-controller.js";
 import { validateSessionMiddleware } from "../../middleware/session-middleware.js";
 import { allowUserJourneyMiddleware } from "../../middleware/allow-user-journey-middleware.js";
 
@@ -10,7 +10,7 @@ router.get(
   PATH_NAMES.ACCOUNT_EXISTS_WITH_PASSKEY,
   validateSessionMiddleware,
   allowUserJourneyMiddleware,
-  getAccountExistsWithPasskey
+  accountExistsWithPasskeyGet
 );
 
 export { router as accountExistsWithPasskeyRouter };
