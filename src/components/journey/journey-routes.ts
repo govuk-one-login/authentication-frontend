101dc84f94bf549ce838f2943d19ca1b89b86d42
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-07-02 16:20:12 +0100

    AUT-5032: Setup the skeleton for the journey-controller
    Extract the event and page from route params and pass them into the getNextPathAndUpdateJourney function
    
    Validation will follow in a subsequent commit

--- src/components/journey/journey-routes.ts
+++ src/components/journey/journey-routes.ts
@@ -1,5 +1,10 @@
 import * as express from "express";
+import { PATH_NAMES } from "../../app.constants.js";
+import { validateSessionMiddleware } from "../../middleware/session-middleware.js";
+import { journeyGet } from "./journey-controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-export { router as journeyRouter }
+router.get(PATH_NAMES.JOURNEY, validateSessionMiddleware, journeyGet);
+
+export { router as journeyRouter };
---
+++ src/components/journey/tests/journey-controller.test.ts
@@ -0,0 +1,75 @@
+import { mockResponse, type RequestOutput, type ResponseOutput } from "mock-req-res";
+import { createMockRequest } from "../../../../test/helpers/mock-request-helper.js";
+import { PATH_NAMES } from "../../../app.constants.js";
+import { expect } from "chai";
+import { describe } from "mocha";
+import { sinon } from "../../../../test/utils/test-utils.js";
+import type { Request, Response } from "express";
+import esmock from "esmock";
+
+describe("journey controller", () => {
+  let req: RequestOutput;
+  let res: ResponseOutput;
+
+  beforeEach(() => {
+    req = createMockRequest(PATH_NAMES.JOURNEY);
+    res = mockResponse();
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe("journeyGet", () => {
+    describe("success", () => {
+      it("should redirect to next page when validation passes", async () => {
+        const previousPage = "/previous-page";
+        const event = "EVENT_TO_GO_NEXT_PAGE";
+        const expectedNextPath = "/next-page";
+
+        req.params = { event, page: "previous-page" };
+        req.session.user = { journey: { nextPath: previousPage, optionalPaths: [] } };
+
+        const { journeyGet, fakeGetNextPathAndUpdateJourney } = await setupStateMachineMock(previousPage, expectedNextPath, event);
+
+        await journeyGet(req as Request, res as Response);
+
+        expect(fakeGetNextPathAndUpdateJourney).to.have.been.calledWith(
+          req,
+          res,
+          event,
+          previousPage
+        );
+        expect(res.redirect).to.have.been.calledWith(expectedNextPath);
+      });
+    });
+  });
+
+  const setupStateMachineMock = async (
+    previousPage: string,
+    expectedNextPath: string,
+    event?: string
+  )=> {
+    const fakeGetNextPathAndUpdateJourney =
+      sinon.fake.resolves(expectedNextPath);
+
+    const { journeyGet } = await esmock("../journey-controller.js", {
+      "../../common/state-machine/state-machine-executor.js": {
+        getNextPathAndUpdateJourney: fakeGetNextPathAndUpdateJourney,
+      },
+      "../../common/state-machine/state-machine.js": {
+        authStateMachine: {
+          states: {
+            [previousPage]: {
+              meta: {
+                allowedEventsFromJourney: event ? [event] : [],
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return { journeyGet, fakeGetNextPathAndUpdateJourney };
+  }
+});
