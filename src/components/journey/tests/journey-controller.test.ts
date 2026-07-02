9503418ea56e3e130006ef8f2f9123c6f4a72d53
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-07-02 17:06:31 +0100

    AUT-5032: Add validation to check that the page from the route params matches the previous page the user was on from the session
    Since the user can enter anything into the browser search bar, we need to add validation around the route params
    
    This checks that the page from route params matches what we think the previous page was in the session. This prevents users from using /journey on pages where it shouldn't be allowed. It also prevents users entering any page and event in the browser and transitioning to that page
    
    See https://govukverify.atlassian.net/wiki/spaces/LO/pages/6574080144/Dynamic+Back+Buttons for more detail

--- src/components/journey/tests/journey-controller.test.ts
+++ src/components/journey/tests/journey-controller.test.ts
@@ -1,4 +1,8 @@
-import { mockResponse, type RequestOutput, type ResponseOutput } from "mock-req-res";
+import {
+  mockResponse,
+  type RequestOutput,
+  type ResponseOutput,
+} from "mock-req-res";
 import { createMockRequest } from "../../../../test/helpers/mock-request-helper.js";
 import { PATH_NAMES } from "../../../app.constants.js";
 import { expect } from "chai";
@@ -28,9 +32,12 @@ describe("journey controller", () => {
         const expectedNextPath = "/next-page";
 
         req.params = { event, page: "previous-page" };
-        req.session.user = { journey: { nextPath: previousPage, optionalPaths: [] } };
+        req.session.user = {
+          journey: { nextPath: previousPage, optionalPaths: [] },
+        };
 
-        const { journeyGet, fakeGetNextPathAndUpdateJourney } = await setupStateMachineMock(previousPage, expectedNextPath, event);
+        const { journeyGet, fakeGetNextPathAndUpdateJourney } =
+          await setupStateMachineMock(previousPage, expectedNextPath, event);
 
         await journeyGet(req as Request, res as Response);
 
@@ -45,10 +52,46 @@ describe("journey controller", () => {
     });
 
     describe("validation", () => {
+      it("should not allow the use of /journey if previous page from session doesn't match previous page in route params", async () => {
+        const previousPageInSession = "/some-page";
+        const previousPageInRouteParams = "other-page";
+        const event = "EVENT_TO_GO_NEXT_PAGE";
+        const expectedNextPath = "/next-page";
+
+        req.params = { event, page: previousPageInRouteParams };
+        req.session.user = {
+          journey: { nextPath: previousPageInSession, optionalPaths: [] },
+        };
+
+        const { journeyGet, fakeGetNextPathAndUpdateJourney } =
+          await setupStateMachineMock(
+            previousPageInRouteParams,
+            expectedNextPath,
+            event
+          );
+
+        await journeyGet(req as Request, res as Response);
+
+        expect(fakeGetNextPathAndUpdateJourney).not.have.been.called;
+        expect(res.redirect).to.have.been.calledWith(previousPageInSession);
+      });
+
       [
-        { page: undefined, event: "SOME_EVENT", description: "page is missing" },
-        { page: "next-page", event: undefined, description: "event is missing" },
-        { page: undefined, event: undefined, description: "both page and event are missing" },
+        {
+          page: undefined,
+          event: "SOME_EVENT",
+          description: "page is missing",
+        },
+        {
+          page: "next-page",
+          event: undefined,
+          description: "event is missing",
+        },
+        {
+          page: undefined,
+          event: undefined,
+          description: "both page and event are missing",
+        },
       ].forEach(({ page, event, description }) => {
         it(`should redirect to previous page when ${description}`, async () => {
           const previousPageInSession = "/next-page";
@@ -70,15 +113,16 @@ describe("journey controller", () => {
 
           expect(fakeGetNextPathAndUpdateJourney).not.have.been.called;
           expect(res.redirect).to.have.been.calledWith(previousPageInSession);
-        })})
-    })
+        });
+      });
+    });
   });
 
   const setupStateMachineMock = async (
     previousPage: string,
     expectedNextPath: string,
     event?: string
-  )=> {
+  ) => {
     const fakeGetNextPathAndUpdateJourney =
       sinon.fake.resolves(expectedNextPath);
 
@@ -100,5 +144,5 @@ describe("journey controller", () => {
     });
 
     return { journeyGet, fakeGetNextPathAndUpdateJourney };
-  }
+  };
 });
