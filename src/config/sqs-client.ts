b15c235efe81b631d8cd4e9d6bfac8b09a126073
Christian Sophocleous <145482922+csophocleous@users.noreply.github.com>
2026-08-27 11:24:12 +0100

    AUT-5707: Create an SqsClient that can be used to send messages to an SQS Queue
    If the SQS_ENDPOINT env var exists, then we set that to be the endpoint of the SqsClient. This will be used for running integration tests. In the case of deployed envs, the clientOptions will be picked up from aws config

--- src/config/sqs-client.ts
+++ src/config/sqs-client.ts
@@ -1,6 +1,5 @@
 import { SQSClient } from "@aws-sdk/client-sqs";
 import { getAwsRegion } from "../config.js";
-import { sendToTXMA } from "@govuk-one-login/event-catalogue-utils";
 
 function getSqsClient(): SQSClient {
   const clientOptions: ConstructorParameters<typeof SQSClient>[0] = {
@@ -18,19 +17,5 @@ function getSqsClient(): SQSClient {
 
   return new SQSClient(clientOptions);
 }
-
-sendToTXMA(
-  "AUTH_PASSKEY_AUTHENTICATION_FAILED",
-  {
-    component_id: "",
-    event_timestamp_ms: 0,
-    timestamp: 0,
-    event_name: "AUTH_PASSKEY_AUTHENTICATION_FAILED",
-  },
-  process.env.SQS_ENDPOINT,
-  {
-    sqsClient: getSqsClient(),
-  }
-);
 
 export const sqsClient = getSqsClient();
