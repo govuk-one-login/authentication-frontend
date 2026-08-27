8d2c81793b12f7f21652da06ef0d9e593ae613b0
Christian Sophocleous <145482922+csophocleous@users.noreply.github.com>
2026-08-27 17:09:13 +0100

    AUT-5707: Lazily initialise the SQS client
    Previously, the sqsClient loaded eagerly. This meant it would be evaluated when imported. However, given that it uses various env vars (namely the SQS_ENDPOINT) which will be set after the sqsClient is imported in integration tests, we need to make the sqsClient load lazily.
    
    This is done by exporting the function that builds the sqsClient on the first call and caches it for reuse. The env vars will then be evaluated when getSqsClient is called rather than at import.

--- src/config/sqs-client.ts
+++ src/config/sqs-client.ts
@@ -1,21 +1,26 @@
 import { SQSClient } from "@aws-sdk/client-sqs";
 import { getAwsRegion } from "../config.js";
 
+let client: SQSClient | undefined
+
 function getSqsClient(): SQSClient {
-  const clientOptions: ConstructorParameters<typeof SQSClient>[0] = {
-    region: getAwsRegion(),
-    maxAttempts: 3,
-  };
-
-  if (process.env.SQS_ENDPOINT) {
-    clientOptions.endpoint = process.env.SQS_ENDPOINT;
-    clientOptions.credentials = {
-      accessKeyId: "na",
-      secretAccessKey: "na",
+  if (!client) {
+    const clientOptions: ConstructorParameters<typeof SQSClient>[0] = {
+      region: getAwsRegion(),
+      maxAttempts: 3,
     };
-  }
 
-  return new SQSClient(clientOptions);
+    if (process.env.SQS_ENDPOINT) {
+      clientOptions.endpoint = process.env.SQS_ENDPOINT;
+      clientOptions.credentials = {
+        accessKeyId: "na",
+        secretAccessKey: "na",
+      };
+    }
+
+    client = new SQSClient(clientOptions);
+  }
+  return client
 }
 
-export const sqsClient = getSqsClient();
+export default getSqsClient;
