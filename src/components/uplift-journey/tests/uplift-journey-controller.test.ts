cfb12f517a201a5fe3b92b91797eee1560699321
Benjamin Fernandes <benjamin.fernandes@digital.cabinet-office.gov.uk>
2026-09-07 12:06:07 +0100

    AUT-5793: Remove redundant addGA
    _ga comes in as a query param from RPs to do cross-domain Google Analytics tracking. This function existed to make sure the query param was passed along correctly on the redirect. However, I don't think that's necessary because we already have crossDomainTrackingMiddleware. This captures _ga whenever it's present as a query param attaches it to the return-to-RP redirect at the end.

--- src/components/uplift-journey/tests/uplift-journey-controller.test.ts
+++ src/components/uplift-journey/tests/uplift-journey-controller.test.ts
@@ -39,22 +39,5 @@ describe("uplift journey controller", () => {
       expect(res.redirect).to.have.been.calledWith(PATH_NAMES.ENTER_MFA);
       expect(fakeService.sendMfaCode).to.have.been.calledOnce;
     });
-
-    it("should send mfa code and redirect to /enter-code with _ga param", async () => {
-      const fakeService: MfaServiceInterface = {
-        sendMfaCode: sinon.fake.returns({
-          success: true,
-        }),
-      } as unknown as MfaServiceInterface;
-
-      req.query._ga = "123123.21321313.2388839";
-
-      await upliftJourneyGet(fakeService)(req as Request, res as Response);
-
-      expect(res.redirect).to.have.been.calledWith(
-        `${PATH_NAMES.ENTER_MFA}?_ga=${req.query._ga}`
-      );
-      expect(fakeService.sendMfaCode).to.have.been.calledOnce;
-    });
   });
 });
